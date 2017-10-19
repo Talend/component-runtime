694db60e051faaf766368368e76855d8886af048
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-10-19 17:14:46 +0200

    one more step to an auto deploy

---
+++ .travis/setup_keys.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mkdir -p ~/.gpg/
+openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
+tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
+for i in priv pub; do
+    cat ~/.gpg/travis.$i | gpg --dearmor > ~/.gpg/travis.$i.bin
+done
