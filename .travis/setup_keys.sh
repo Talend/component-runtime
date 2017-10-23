e1b198b75c148e2556ec82815e9c8f76af4830a5
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-10-23 16:37:30 +0200

    don't extract keys on PR

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+    exit 0
+fi
+
 mkdir -p ~/.gpg/
 openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
 tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
