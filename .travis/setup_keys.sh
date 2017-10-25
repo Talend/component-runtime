e9c894476ee3f87a83873fe40f3aea623c441b92
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-10-25 08:52:10 +0200

    ensure key extraction is not done for PR branches

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_3e1475539c2f_key" = "x" ]; then
     exit 0
+else
+    mkdir -p ~/.gpg/
+    openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
+    tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
+    for i in priv pub; do
+        cat ~/.gpg/travis.$i | gpg --dearmor > ~/.gpg/travis.$i.bin
+    done
 fi
-
-mkdir -p ~/.gpg/
-openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
-tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
-for i in priv pub; do
-    cat ~/.gpg/travis.$i | gpg --dearmor > ~/.gpg/travis.$i.bin
-done
