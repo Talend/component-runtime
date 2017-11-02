dd9031e7d6f925a76c0c33603138eb819c6a9846
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-11-02 16:20:11 +0100

    removing done todo

--- component-server/src/main/java/org/talend/sdk/component/server/configuration/ComponentServerConfiguration.java
+++ component-server/src/main/java/org/talend/sdk/component/server/configuration/ComponentServerConfiguration.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.talend.sdk.component.server.configuration;
 
@@ -23,10 +23,10 @@ import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.apache.deltaspike.core.api.config.Configuration;
 import org.talend.sdk.component.api.meta.Documentation;
 
-// TODO: enhance documentation with that generated
 @ApplicationScoped
 @Configuration(prefix = "talend.component.server.")
 public interface ComponentServerConfiguration {
+
     @Documentation("The local maven repository used to locate components and their dependencies")
     @ConfigProperty(name = "maven.repository", defaultValue = "${user.home}/.m2/repository")
     String mavenRepository();
