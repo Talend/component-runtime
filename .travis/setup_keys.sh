8a2dbedb2eea781cbcfee80a6445cab8e8fcd956
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-11-07 10:06:18 +0100

    fixing the setup_keys script - bad copy/paste form api repo

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_3e1475539c2f_key" = "x" ]; then
-    exit 0
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x" ]; then
+    echo "Skipping deployment setup since we are building a pull request"
 else
     mkdir -p ~/.gpg/
     openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
