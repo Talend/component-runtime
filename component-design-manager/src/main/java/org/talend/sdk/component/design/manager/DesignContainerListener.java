cd174c4234ebafa1a18488d1410010b5fd1baa8d
Ivan Honchar <ivan.honchar@globallogic.com>
2017-11-13 20:53:28 +0200

    feat(TCOMP-680): implement DesignContainerListener

--- component-design-manager/src/main/java/org/talend/sdk/component/design/manager/DesignContainerListener.java
+++ component-design-manager/src/main/java/org/talend/sdk/component/design/manager/DesignContainerListener.java
@@ -15,7 +15,12 @@
  */
 package org.talend.sdk.component.design.manager;
 
+import static java.util.Optional.ofNullable;
+
+import java.util.stream.Stream;
+
 import org.talend.sdk.component.container.Container;
+import org.talend.sdk.component.runtime.manager.ContainerComponentRegistry;
 import org.talend.sdk.component.runtime.manager.spi.ContainerListenerExtension;
 
 /**
@@ -23,12 +28,41 @@ import org.talend.sdk.component.runtime.manager.spi.ContainerListenerExtension;
  */
 public class DesignContainerListener implements ContainerListenerExtension {
 
+    /**
+     * Enriches {@link Container} with {@link DesignModelRegistry}
+     * It depends on Updater listener which adds {@link ContainerComponentRegistry} class to {@link Container}
+     */
     @Override
     public void onCreate(Container container) {
+        ContainerComponentRegistry componentRegistry = container.get(ContainerComponentRegistry.class);
+        if (componentRegistry == null) {
+            throw new IllegalArgumentException("container doesn't contain ContainerComponentRegistry");
+        }
+
+        DesignModelRegistry models = new DesignModelRegistry();
+
+        componentRegistry.getComponents().values().stream()
+                .flatMap(family -> Stream.concat( //
+                        family.getPartitionMappers().values().stream(), //
+                        family.getProcessors().values().stream()) //
+                ).forEach(meta -> models.getModels().put(meta.getId(), new DesignModel( //
+                        meta.getId(), //
+                        meta.getName(), //
+                        meta.getInputFlows(), //
+                        meta.getOutputFlows()))); //
+
+        container.set(DesignModelRegistry.class, models);
     }
 
+    /**
+     * Removes {@link DesignModelRegistry} from {@link Container} and cleans it
+     */
     @Override
     public void onClose(Container container) {
+        ofNullable(container.get(DesignModelRegistry.class)).ifPresent(r -> {
+            final DesignModelRegistry registry = container.remove(DesignModelRegistry.class);
+            registry.getModels().clear();
+        });
     }
 
 }
