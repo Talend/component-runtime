5352cd8196ed3fd835a33b2fb7060ae087a821c7
Anas KHABALI <akhabali@talend.com>
2017-11-17 09:46:59 +0100

    add configurationtype metadata to ConfigurationType annotation elements

--- component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/reflect/parameterenricher/ConfigurationTypeParameterEnricher.java
+++ component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/reflect/parameterenricher/ConfigurationTypeParameterEnricher.java
@@ -1,32 +1,34 @@
 /**
- *  Copyright (C) 2006-2017 Talend Inc. - www.talend.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.talend.sdk.component.runtime.manager.reflect.parameterenricher;
-
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.singletonMap;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.talend.sdk.component.api.configuration.type.meta.ConfigurationType;
 import org.talend.sdk.component.spi.parameter.ParameterExtensionEnricher;
 
+import static java.util.Collections.emptyMap;
+
 public class ConfigurationTypeParameterEnricher implements ParameterExtensionEnricher {
+
+    public static final String META_PREFIX = "tcomp::configurationtype::";
 
     @Override
     public Map<String, String> onParameterAnnotation(final String parameterName, final Type parameterType,
@@ -36,7 +38,12 @@ public class ConfigurationTypeParameterEnricher implements ParameterExtensionEnr
             final String type = configType.value();
             final String name = getName(annotation);
             if (name != null) {
-                return singletonMap(/* not prefixed by tcomp:: since it is a core/direct metadata */type, name);
+                return new HashMap<String, String>() {{
+                    put(META_PREFIX + "type", type);
+                    put(META_PREFIX + "name", name);
+                    put(META_PREFIX + "displayName",
+                            name); // todo: use resource bundle "family.configutype.type._displayName=xxx"
+                }};
             }
         }
         return emptyMap();
--- component-runtime-manager/src/test/java/org/talend/sdk/component/runtime/manager/reflect/parameterenricher/ConfigurationTypeParameterEnricherTest.java
+++ component-runtime-manager/src/test/java/org/talend/sdk/component/runtime/manager/reflect/parameterenricher/ConfigurationTypeParameterEnricherTest.java
@@ -1,35 +1,39 @@
 /**
- *  Copyright (C) 2006-2017 Talend Inc. - www.talend.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.talend.sdk.component.runtime.manager.reflect.parameterenricher;
 
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.singletonMap;
-import static org.junit.Assert.assertEquals;
-
 import java.lang.annotation.Annotation;
+import java.util.HashMap;
 
 import org.junit.Test;
 import org.talend.sdk.component.api.configuration.type.DataSet;
+
+import static java.util.Collections.emptyMap;
+import static org.junit.Assert.assertEquals;
 
 public class ConfigurationTypeParameterEnricherTest {
 
     @Test
     public void readConfigTypes() {
         final ConfigurationTypeParameterEnricher enricher = new ConfigurationTypeParameterEnricher();
-        assertEquals(singletonMap("dataset", "test"), enricher.onParameterAnnotation("testParam", null, new DataSet() {
+        assertEquals(new HashMap<String, String>() {{
+            put("tcomp::configurationtype::type", "dataset");
+            put("tcomp::configurationtype::name", "test");
+            put("tcomp::configurationtype::displayName", "test");
+        }}, enricher.onParameterAnnotation("testParam", null, new DataSet() {
 
             @Override
             public Class<? extends Annotation> annotationType() {
