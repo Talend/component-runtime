11c28b3508c00b7b179c38ca6a31a948f74ed4a0
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-11-20 16:14:01 +0100

    fixing the service class name

--- component-starter-server/src/main/resources/generator/component/Service.java
+++ component-starter-server/src/main/resources/generator/component/Service.java
@@ -3,7 +3,7 @@ package {{package}};
 import org.talend.sdk.component.api.service.Service;
 
 @Service
-public class SalesforceService {
+public class {{className}} {
 
     // you can put logic here you can reuse in components
 
