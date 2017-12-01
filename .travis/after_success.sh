a380cf5bc1e326d7dd88535169a055172e0ec567
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-01 09:39:42 +0100

    skip hub-detect until we actually want/can to run it

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -8,11 +8,9 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x"
     exit 0
 fi
 
-export MAVEN_OPTS="-DTLND_BLACKDUCK_PASS=${TLND_BLACKDUCK_PASS:-skip} -DTLND_BLACKDUCK_USER=${TLND_BLACKDUCK_USER:-skip}"
-
 OPTS="--batch-mode --settings $PWD/.travis/settings.xml"
 
-mvn clean deploy verify -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
+mvn clean deploy verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
 
 cd documentation
     mvn -Dgithub.site.profile=latest clean package pre-site -Pgh-pages $OPTS
