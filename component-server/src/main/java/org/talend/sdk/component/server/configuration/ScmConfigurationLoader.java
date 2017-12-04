66b3dccf7b3f7a683ae57d80914c69e000b3e313
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-04 10:28:46 +0100

    environment endpoint to know which version we are relying on as a client

---
+++ component-server/src/main/java/org/talend/sdk/component/server/configuration/ScmConfigurationLoader.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.server.configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.deltaspike.core.api.config.Source;
+import org.apache.deltaspike.core.impl.config.MapConfigSource;
+
+@Source
+@ApplicationScoped
+public class ScmConfigurationLoader extends MapConfigSource {
+
+    public ScmConfigurationLoader() {
+        super(new HashMap<>());
+    }
+
+    @PostConstruct
+    private void init() {
+        try (final InputStream stream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("TALEND-INF/git.properties")) {
+            final Properties properties = new Properties() {
+
+                {
+                    try {
+                        load(stream);
+                    } catch (final IOException e) {
+                        throw new IllegalStateException(e);
+                    }
+                }
+            };
+            properties.stringPropertyNames().stream().collect(this::getProperties,
+                    (m, k) -> m.put(k, properties.getProperty(k)), Map::putAll);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public String getConfigName() {
+        return "component-scm-configuration";
+    }
+}
---
+++ component-server/src/test/java/org/talend/sdk/component/server/front/EnvironmentResourceTest.java
@@ -0,0 +1,43 @@
+/**
+ *  Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.talend.sdk.component.server.front;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+import javax.inject.Inject;
+import javax.ws.rs.client.WebTarget;
+
+import org.apache.meecrowave.junit.MonoMeecrowave;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.talend.sdk.component.server.front.model.Environment;
+
+@RunWith(MonoMeecrowave.Runner.class)
+public class EnvironmentResourceTest {
+    @Inject
+    private WebTarget base;
+
+    @Test
+    public void environment() {
+        final Environment environment = base.path("environment").request(APPLICATION_JSON_TYPE).get(Environment.class);
+        assertEquals(1, environment.getLatestApiVersion());
+        Stream.of(environment.getCommit(), environment.getTime(), environment.getVersion())
+                .forEach(Assert::assertNotNull);
+    }
+}
