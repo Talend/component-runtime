af804f6a7392829577df39f51056621c54b58bb7
Anas KHABALI <akhabali@talend.com>
2017-12-06 15:42:05 +0000

    add possibility to translate config using the class name

--- component-runtime-impl/src/main/java/org/talend/sdk/component/runtime/internationalization/ParameterBundle.java
+++ component-runtime-impl/src/main/java/org/talend/sdk/component/runtime/internationalization/ParameterBundle.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,11 +20,18 @@ import java.util.ResourceBundle;
 
 public class ParameterBundle extends InternalBundle {
 
-    public ParameterBundle(final ResourceBundle bundle, final String prefix) {
+    private final String simpleName;
+
+    public ParameterBundle(final ResourceBundle bundle, final String prefix, final String simpleName) {
         super(bundle, prefix);
+        this.simpleName = simpleName;
     }
 
     public Optional<String> displayName() {
         return readValue("_displayName");
+    }
+
+    public Optional<String> displayName(final String child) {
+        return readRawValue(simpleName + "." + child + "._displayName");
     }
 }
--- component-server/src/main/java/org/talend/sdk/component/server/service/PropertiesService.java
+++ component-server/src/main/java/org/talend/sdk/component/server/service/PropertiesService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,11 @@
  */
 package org.talend.sdk.component.server.service;
 
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toMap;
-
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Stream;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -33,16 +29,24 @@ import org.talend.sdk.component.runtime.manager.tools.DefaultValueInspector;
 import org.talend.sdk.component.server.front.model.PropertyValidation;
 import org.talend.sdk.component.server.front.model.SimplePropertyDefinition;
 
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toMap;
+
 @ApplicationScoped
 public class PropertiesService {
+
+    private final DefaultValueInspector defaultValueInspector = new DefaultValueInspector();
 
     @Inject
     private PropertyValidationService propertyValidationService;
 
-    private final DefaultValueInspector defaultValueInspector = new DefaultValueInspector();
-
     public Stream<SimplePropertyDefinition> buildProperties(final Collection<ParameterMeta> meta,
             final ClassLoader loader, final Locale locale, final Object rootInstance) {
+        return buildProperties(meta, loader, locale, rootInstance, null);
+    }
+
+    private Stream<SimplePropertyDefinition> buildProperties(final Collection<ParameterMeta> meta,
+            final ClassLoader loader, final Locale locale, final Object rootInstance, final ParameterMeta parent) {
         return meta.stream().flatMap(p -> {
             final String path = sanitizePropertyName(p.getPath());
             final String name = sanitizePropertyName(p.getName());
@@ -60,11 +64,13 @@ public class PropertiesService {
             final Object instance = defaultValueInspector.createDemoInstance(rootInstance, p);
             return Stream.concat(
                     Stream.of(new SimplePropertyDefinition(path, name,
-                            p.findBundle(loader, locale).displayName().orElse(name), type,
-                            defaultValueInspector.findDefault(instance, p), validation, metadata)),
-                    buildProperties(p.getNestedParameters(), loader, locale, instance));
+                            p.findBundle(loader, locale).displayName()
+                                    .orElseGet(() -> parent == null ? name
+                                            : parent.findBundle(loader, locale).displayName(p.getName()).orElse(name)),
+                            type, defaultValueInspector.findDefault(instance, p), validation, metadata)),
+                    buildProperties(p.getNestedParameters(), loader, locale, instance, p));
         }).sorted(Comparator.comparing(SimplePropertyDefinition::getPath)); // important cause it is the way you want to
-                                                                            // see it
+        // see it
     }
 
     private String sanitizePropertyName(final String path) {
