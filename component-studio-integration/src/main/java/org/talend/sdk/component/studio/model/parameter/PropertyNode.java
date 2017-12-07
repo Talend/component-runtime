40a613010ad2f4808751a7612b7592cbef7b7f9a
Ivan Honchar <ivan.honchar@globallogic.com>
2017-12-08 01:18:10 +0200

    feat(TCOMP-681): implement optionsorder and gridlayout order in PropertyNode

--- component-studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/PropertyNode.java
+++ component-studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/PropertyNode.java
@@ -15,12 +15,15 @@
  */
 package org.talend.sdk.component.studio.model.parameter;
 
+import static org.talend.sdk.component.studio.model.parameter.Metadatas.ORDER_SEPARATOR;
 import static org.talend.sdk.component.studio.model.parameter.Metadatas.UI_GRIDLAYOUT_ADVANCED;
 import static org.talend.sdk.component.studio.model.parameter.Metadatas.UI_GRIDLAYOUT_MAIN;
+import static org.talend.sdk.component.studio.model.parameter.Metadatas.UI_OPTIONS_ORDER;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -111,8 +114,11 @@ public class PropertyNode {
      */
     private List<PropertyNode> getMainChildren() {
         Set<String> mainChildrenNames = getMainChildrenNames();
-        return children.stream().filter(node -> mainChildrenNames.contains(node.property.getName())).collect(
-                Collectors.toList());
+        List<PropertyNode> mainChildren =
+                children.stream().filter(node -> mainChildrenNames.contains(node.property.getName())).collect(
+                        Collectors.toList());
+        sortMain(mainChildren);
+        return mainChildren;
     }
 
     private Set<String> getMainChildrenNames() {
@@ -149,8 +155,11 @@ public class PropertyNode {
      */
     private List<PropertyNode> getAdvancedChildren() {
         Set<String> advancedChildrenNames = getAdvancedChildrenNames();
-        return children.stream().filter(node -> advancedChildrenNames.contains(node.property.getName())).collect(
-                Collectors.toList());
+        List<PropertyNode> advancedChildren =
+                children.stream().filter(node -> advancedChildrenNames.contains(node.property.getName())).collect(
+                        Collectors.toList());
+        sortAdvanced(advancedChildren);
+        return advancedChildren;
     }
 
     private Set<String> getAdvancedChildrenNames() {
@@ -160,5 +169,77 @@ public class PropertyNode {
             return new HashSet<>(Arrays.asList(names));
         }
         return Collections.emptySet();
+    }
+
+    /**
+     * Sorts main children sublist
+     * 
+     * @param nodes
+     */
+    private void sortMain(final List<PropertyNode> nodes) {
+        String gridLayout = property.getMetadata().get(UI_GRIDLAYOUT_MAIN);
+        if (gridLayout != null) {
+            sort(nodes, getGridLayoutOrder(gridLayout));
+            return;
+        }
+        String optionsOrder = property.getMetadata().get(UI_OPTIONS_ORDER);
+        if (optionsOrder != null) {
+            sort(nodes, getOptionsOrder(optionsOrder));
+            return;
+        }
+        // else do not sort
+        return;
+    }
+
+    private void sortAdvanced(final List<PropertyNode> nodes) {
+        String gridLayout = property.getMetadata().get(UI_GRIDLAYOUT_ADVANCED);
+        if (gridLayout != null) {
+            sort(nodes, getGridLayoutOrder(gridLayout));
+        }
+        // else do nothing
+    }
+
+    /**
+     * Computes order for comparator accoding gridlayout
+     * 
+     * @param optionsOrder metadata value for ui::optionsorder::value
+     * @return order
+     */
+    private HashMap<String, Integer> getGridLayoutOrder(final String gridLayout) {
+        String[] values = gridLayout.split(",|\\|");
+        HashMap<String, Integer> order = new HashMap<>();
+        for (int i = 0; i < values.length; i++) {
+            order.put(values[i], i);
+        }
+        return order;
+    }
+
+    /**
+     * Sorts according specified order
+     * 
+     * @param nodes
+     * @param order
+     */
+    private void sort(final List<PropertyNode> nodes, final HashMap<String, Integer> order) {
+        nodes.sort((node1, node2) -> {
+            Integer i1 = order.get(node1.getProperty().getName());
+            Integer i2 = order.get(node2.getProperty().getName());
+            return i1.compareTo(i2);
+        });
+    }
+
+    /**
+     * Computes order for comparator according optionsorder
+     * 
+     * @param optionsOrder metadata value for ui::optionsorder::value
+     * @return order
+     */
+    private HashMap<String, Integer> getOptionsOrder(final String optionsOrder) {
+        String[] values = optionsOrder.split(ORDER_SEPARATOR);
+        HashMap<String, Integer> order = new HashMap<>();
+        for (int i = 0; i < values.length; i++) {
+            order.put(values[i], i);
+        }
+        return order;
     }
 }
