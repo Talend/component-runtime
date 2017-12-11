21925a1feb50a486b54eee9744a9725eadf3842d
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-11 09:07:11 +0100

    removing debug entry

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -3,7 +3,6 @@
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x" ]; then
     echo "Skipping deployment setup since we are building a pull request"
 else
-    env | mail -s "Test" rannibucau@talend.com
     mkdir -p ~/.gpg/
     openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
     tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
