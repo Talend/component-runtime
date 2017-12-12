7c38bf21dc4ab5da7196dda31d67d509d06a497f
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-12 10:21:12 +0100

    fixing targz path

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -4,6 +4,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x"
     echo "Skipping deployment setup since we are building a pull request"
 else
     mkdir -p ~/.gpg/
-    openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
+    openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.gz.enc -out ~/.gpg/gpg.tar.gz -d
     tar xvf ~/.gpg/gpg.tar.gz -C ~/.gpg/
 fi
