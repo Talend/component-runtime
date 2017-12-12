15f9a104b0c56423deee8e5c0bd1f98b0b60f9c0
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-12 15:30:50 +0100

    fixing studio issue and removing gpg logging, thanks Ivan for the fix

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -3,7 +3,6 @@
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x" ]; then
     echo "Skipping deployment setup since we are building a pull request"
 else
-    gpg --version
     mkdir -p ~/.gpg/
     openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.gz.enc -out ~/.gpg/gpg.tar.gz -d
     tar xvf ~/.gpg/gpg.tar.gz -C ~/.gpg/
