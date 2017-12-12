1c2faf745cad1c3e2e970c8f1af712d0165f0aae
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-12 10:17:46 +0100

    using directly bin gpg keys

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -5,8 +5,5 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x"
 else
     mkdir -p ~/.gpg/
     openssl aes-256-cbc -K $encrypted_03e441b90173_key -iv $encrypted_03e441b90173_iv -in .travis/gpg/gpg.tar.enc -out ~/.gpg/gpg.tar -d
-    tar xvf ~/.gpg/gpg.tar -C ~/.gpg/
-    for i in priv pub; do
-        cat ~/.gpg/travis.$i | gpg --dearmor > ~/.gpg/travis.$i.bin
-    done
+    tar xvf ~/.gpg/gpg.tar.gz -C ~/.gpg/
 fi
