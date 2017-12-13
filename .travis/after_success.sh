ece67193a5aab44a515f660762c77216dd652597
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-13 14:14:11 +0100

    trying to split blackduck and nexus deployment in 2 phases while blackduck returns errors

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -10,8 +10,17 @@ fi
 
 OPTS="--batch-mode --settings $PWD/.travis/settings.xml"
 
-mvn clean deploy verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
+mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
+deployStatus=$(echo $?)
+
+mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS
+hubDetectStatus=$(echo $?)
 
 cd documentation
     mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
+    docStatus=$(echo $?)
 cd -
+
+echo "Deploy        : ${deployStatus}"
+echo "Blackduck     : ${hubDetectStatus}"
+echo "Documentation : ${docStatus}"
