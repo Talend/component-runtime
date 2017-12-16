a4adbb36747035f015a531b3589897787451ba33
Ivan Honchar <ivan.honchar@globallogic.com>
2017-12-16 07:21:14 +0200

    chore(TCOMP-681): remove unused Listener implementation from TaCoKitConfigurationWizardPage

--- component-studio-integration/src/main/java/org/talend/sdk/component/studio/ui/wizard/page/TaCoKitConfigurationWizardPage.java
+++ component-studio-integration/src/main/java/org/talend/sdk/component/studio/ui/wizard/page/TaCoKitConfigurationWizardPage.java
@@ -12,8 +12,6 @@
  */
 package org.talend.sdk.component.studio.ui.wizard.page;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +24,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.talend.core.model.process.EComponentCategory;
 import org.talend.core.model.process.Element;
-import org.talend.designer.core.generic.constants.IElementParameterEventProperties;
 import org.talend.designer.core.model.FakeElement;
 import org.talend.designer.core.model.components.DummyComponent;
 import org.talend.designer.core.model.components.ElementParameter;
@@ -46,7 +43,7 @@ import org.talend.sdk.component.studio.ui.wizard.TaCoKitConfigurationRuntimeData
 /**
  * DOC cmeng class global comment. Detailled comment
  */
-public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage implements PropertyChangeListener {
+public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage {
 
     private Element element;
 
@@ -92,7 +89,6 @@ public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage impleme
         tacokitComposite = new TaCoKitWizardComposite(container, SWT.H_SCROLL | SWT.V_SCROLL | SWT.NO_FOCUS,
                 EComponentCategory.BASIC, element, configurationModel, true, container.getBackground());
         tacokitComposite.setLayoutData(createMainFormData(addContextFields));
-        tacokitComposite.addPropertyChangeListener(this);
         tacokitComposite.addValueChangedListener(new IValueChangedListener() {
 
             @Override
@@ -128,19 +124,6 @@ public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage impleme
     @Override
     protected IStatus[] getStatuses() {
         return Arrays.asList(super.getStatuses(), tocokitConfigStatus).toArray(new IStatus[0]);
-    }
-
-    @Override
-    public void propertyChange(final PropertyChangeEvent evt) {
-        String propertyName = evt.getPropertyName();
-        if (IElementParameterEventProperties.EVENT_PROPERTY_NAME_CHANGED.equals(propertyName)) {
-            String newPropertyName = String.valueOf(evt.getNewValue());
-            updateProperty(newPropertyName);
-        }
-    }
-
-    private void updateProperty(final String newPropertyName) {
-        // TODO
     }
 
 }
