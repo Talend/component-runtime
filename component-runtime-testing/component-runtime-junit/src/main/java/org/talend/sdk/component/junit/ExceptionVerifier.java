71daffc6777b86c2ae29968fb8a64e5dd012d943
Anas KHABALI <akhabali@talend.com>
2017-12-19 08:42:19 +0000

    add javadoc and copyright header

--- component-runtime-testing/component-runtime-junit/src/main/java/org/talend/sdk/component/junit/ExceptionVerifier.java
+++ component-runtime-testing/component-runtime-junit/src/main/java/org/talend/sdk/component/junit/ExceptionVerifier.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.talend.sdk.component.junit;
 
 import static org.junit.Assert.fail;
@@ -9,17 +24,21 @@ import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
 /**
- * A rule to access an exception from a test using a Consumer<HttpException>.
- * <code>
- *
- * @Rule public ExceptionVerifier<HttpException> httpExceptionRule = new ExceptionVerifier<>();
- * ...
- * httpExceptionRule.assertWith(e -> {
- * assertEquals(401, e.getResponse().status());
- * assertEquals("expected error message", e.getResponse().error(String.class));
- * });
- *
- * </code>
+ * A rule to access an exception from a test using a {@link Consumer}
+ * Usage example :
+ * 
+ * <pre>
+ * {@code
+ * Rule public ExceptionVerifier<HttpException> httpExceptionRule = new ExceptionVerifier<>();
+ *  &#64;Test
+ *  public void test(){
+ *      httpExceptionRule.assertWith(e -> {
+ *          assertEquals(401, e.getResponse().status());
+ *          assertEquals("expected error message", e.getResponse().error(String.class));
+ *      });
+ *  }
+ * }
+ * </pre>
  */
 public class ExceptionVerifier<T extends RuntimeException> implements TestRule {
 
