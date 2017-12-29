4e01118646e78574628bd1c20838d67c465fcba7
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-29 17:41:18 +0100

    revert the parallelism in after_success since it breaks travis timeout

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,29 +9,9 @@ fi
 
 OPTS="--batch-mode --settings $PWD/.travis/settings.xml"
 
-# clean the currently built data to be able to copy the project N times and build it faster
-work_dir=../after_success_workdir
-mvn clean
+mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
+mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS
 
-for i in deploy blackduck documentation; do
-    rm -Rf $work_dir/$i
-    mkdir -p $work_dir/$i
-    time cp -r . $work_dir/$i && echo "Copied the source repository for $i"
-done
-
-
-# run in parallel the OSS snapshot deployment, the blackduck scanning and the doc deployment
-cd $work_dir/deploy
-    mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS &
+cd documentation
+    mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
 cd -
-
-cd $work_dir/blackduck
-    mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS &
-cd -
-
-cd $work_dir/documentation/documentation
-    mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS &
-cd -
-
-# wait they are all done
-wait
--- .travis/install.sh
+++ .travis/install.sh
@@ -6,10 +6,9 @@ mvn dependency:resolve dependency:resolve-plugins
 # front
 for i in component-form/component-form-demo component-starter-server component-tools; do
     cd $i
-        mvn frontend:install-node-and-yarn@install-node-and-yarn frontend:yarn@yarn-install &
+        mvn frontend:install-node-and-yarn@install-node-and-yarn frontend:yarn@yarn-install
     cd -
 done
-wait
 
 # studio (p2)
 cd component-studio-integration
