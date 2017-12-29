c8940a2830bb6a3ec5307c6bfe9851f07817bb58
Romain Manni-Bucau <rmannibucau@gmail.com>
2017-12-29 16:09:58 +0100

    trying to parallelize the after_success work

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -10,14 +10,29 @@ fi
 
 OPTS="--batch-mode --settings $PWD/.travis/settings.xml"
 
-function _run() {
-    echo $(date +%X)
-    eval $@ && echo "Status: $?"
-    echo $(date +%X)
-}
-_run mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
-_run mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS
+# clean the currently built data to be able to copy the project N times and build it faster
+work_dir=../after_success_workdir
+mvn clean
 
-cd documentation
-    _run mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
+for i in deploy blackduck documentation; do
+    rm -Rf $work_dir/$i
+    mkdir -p $work_dir/$i
+    time cp -r . $work_dir/$i && echo "Copied the source repository for $i"
+done
+
+
+# run in parallel the OSS snapshot deployment, the blackduck scanning and the doc deployment
+cd $work_dir/deploy
+    mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS &
 cd -
+
+cd $work_dir/blackduck
+    mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS &
+cd -
+
+cd $work_dir/documentation/documentation
+    mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS &
+cd -
+
+# wait they are all done
+wait
