804f4e5d3e05b7257f14b59bc4faf01db21194a7
Romain Manni-Bucau <rmannibucau@apache.org>
2017-12-31 12:41:02 +0100

    adding travis_wait for after_success

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,9 +9,9 @@ fi
 
 OPTS="--batch-mode --settings $PWD/.travis/settings.xml"
 
-mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
-mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS
+travis_wait 60 mvn clean deploy -Dhub-detect.skip=true -DskipTests -Dinvoker.skip=true -Possrh -Prelease $OPTS
+travis_wait 60 mvn clean verify -Dhub-detect.skip=false -DskipTests -Dinvoker.skip=true -Dskip.yarn=true -Possrh -Prelease $OPTS
 
 cd documentation
-    mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
+    travis_wait 60 mvn clean verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
 cd -
