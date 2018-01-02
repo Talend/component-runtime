12f4a5002906754ad27a859315f2bfa94a5cc85a
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-01-02 09:34:05 +0100

    less logs for the dependency resolution

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # maven
-mvn dependency:resolve dependency:resolve-plugins
+MAVEN_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=warn" mvn dependency:resolve dependency:resolve-plugins
 
 # front
 for i in component-form/component-form-demo component-starter-server component-tools; do
