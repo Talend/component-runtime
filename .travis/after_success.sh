27dd6bf811d792d8603df91da8e47de988d560c5
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-01-10 09:49:09 +0100

    adding back clean in after_success to avoid to fail on shades

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,8 +9,8 @@ fi
 
 OPTS="-Dcheckstyle.skip=true -Drat.skip=true -DskipTests -Dinvoker.skip=true -Dskip.yarn=true --batch-mode --settings $PWD/.travis/settings.xml  -Possrh -Prelease"
 
-node .travis/heartbeat.js mvn deploy -Dhub-detect.skip=true $OPTS
-node .travis/heartbeat.js mvn verify -Dhub-detect.skip=false $OPTS
+node .travis/heartbeat.js mvn clean deploy -Dhub-detect.skip=true $OPTS
+node .travis/heartbeat.js mvn clean verify -Dhub-detect.skip=false $OPTS
 
 cd documentation
     node ../.travis/heartbeat.js mvn verify -Pgh-pages -Dgithub.site.profile=latest $OPTS
