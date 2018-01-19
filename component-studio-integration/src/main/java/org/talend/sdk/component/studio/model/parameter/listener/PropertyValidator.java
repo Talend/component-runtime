3c4783a44170e24971d7c7d0c71cc786ce64f1a2
Ivan Honchar <ivan.honchar@globallogic.com>
2018-01-19 17:59:09 +0200

    fix(TCOMP-681): skip contextual value from validation

--- component-studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/listener/PropertyValidator.java
+++ component-studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/listener/PropertyValidator.java
@@ -17,6 +17,7 @@ package org.talend.sdk.component.studio.model.parameter.listener;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.regex.Pattern;
 
 import org.talend.core.model.process.EParameterFieldType;
 import org.talend.sdk.component.studio.model.parameter.ValidationLabel;
@@ -26,6 +27,8 @@ import org.talend.sdk.component.studio.model.parameter.ValidationLabel;
  * it activates {@code label} ElementParameter and sets its validation message
  */
 public abstract class PropertyValidator implements PropertyChangeListener {
+
+    private final static Pattern CONTEXT_PATTERN = Pattern.compile("^(context\\.).*");
 
     private final ValidationLabel label;
 
@@ -39,13 +42,54 @@ public abstract class PropertyValidator implements PropertyChangeListener {
         this.validationMessage = validationMessage;
     }
 
+    /**
+     * Validates new value, if it is raw value. Skips validation for contextual value
+     * 
+     * @param event property change event, which provides new value to validate
+     */
     @Override
     public void propertyChange(final PropertyChangeEvent event) {
+        if (isContextualValue(event.getNewValue())) {
+            label.hide(validationMessage);
+            return;
+        }
         if (!validate(event.getNewValue())) {
             label.show(validationMessage);
         } else {
             label.hide(validationMessage);
         }
+    }
+
+    /**
+     * Checks whether {@code value} is raw data or contains {@code context} variable.
+     * It is assumed that any not String value is raw data.
+     * The {@code value} contains {@code context} if some of words in it starts with "context."
+     * 
+     * @param value value to check
+     * @return true, if value contains {@code context} variables
+     */
+    protected boolean isContextualValue(final Object value) {
+        if (!String.class.isInstance(value)) {
+            return false;
+        }
+        String strValue = (String) value;
+        String[] tokens = strValue.split(" ");
+        for (String token : tokens) {
+            if (isContextVariable(token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether incoming token is context variable.
+     * 
+     * @param token token to check
+     * @return true, it the token is context variable
+     */
+    private boolean isContextVariable(final String token) {
+        return CONTEXT_PATTERN.matcher(token).matches();
     }
 
     abstract boolean validate(final Object newValue);
