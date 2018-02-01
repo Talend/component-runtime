ed820c38b239565cd301f1cecb14eba12decd413
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-02-01 11:11:00 +0100

    fixing import after jsonobject branch merge

--- component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/chain/ExecutionChain.java
+++ component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/chain/ExecutionChain.java
@@ -21,6 +21,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
