34f286b59102c5237b10cc42e851b253e3f6996d
Anas KHABALI <akhabali@talend.com>
2018-02-02 16:41:34 +0000

    add licence header to class

--- component-runtime-testing/component-runtime-beam-junit/src/test/java/org/talend/sdk/component/junit/beam/ProcessorTest.java
+++ component-runtime-testing/component-runtime-beam-junit/src/test/java/org/talend/sdk/component/junit/beam/ProcessorTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.talend.sdk.component.junit.beam;
 
 import static java.util.Arrays.asList;
@@ -45,7 +60,7 @@ public class ProcessorTest {
                         asList(/* TODO - list of your input data for this branch. Instances of CompanyComponent4DefaultInput.class */));
 
         // Convert it to a beam "source"
-        //fixme 
+        //fixme
         //        final MapCoder<String, List<Serializable>> coder = MapCoder.of(StringUtf8Coder.of(),
         //                ListCoder.of(SerializableCoder.of(Serializable.class)));
         //        final PCollection<Map<String, List<Serializable>>> inputs = pipeline.apply(
