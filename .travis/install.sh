6a2351fef3ee8cd349669f9d811a189ec52b2809
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-02-05 10:19:09 +0100

    fixing install.sh after form-demo drop

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ echo "Resolving maven dependencies and plugins"
 MAVEN_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=warn $MAVEN_OPTS" mvn dependency:resolve dependency:resolve-plugins --batch-mode -e -q
 
 # front
-for i in component-form/component-form-demo component-starter-server component-tools; do
+for i in component-tools-webapp component-starter-server component-tools; do
     cd $i
         mvn frontend:install-node-and-yarn@install-node-and-yarn frontend:yarn@yarn-install --batch-mode -e -q
     cd -
