fa2579feb96a3472544131a14467fbcb83649f5d
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-03-29 14:28:11 +0200

    beamdiextension cant inherit from beam one ATM

--- component-runtime-di/src/main/java/org/talend/sdk/component/runtime/di/beam/BeamDiExtension.java
+++ component-runtime-di/src/main/java/org/talend/sdk/component/runtime/di/beam/BeamDiExtension.java
@@ -15,21 +15,43 @@
  */
 package org.talend.sdk.component.runtime.di.beam;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.util.Collection;
+import java.util.Map;
+
 import org.talend.sdk.component.runtime.beam.spi.BeamComponentExtension;
 import org.talend.sdk.component.runtime.di.beam.components.QueueMapper;
 import org.talend.sdk.component.runtime.di.beam.components.QueueOutput;
 import org.talend.sdk.component.runtime.input.Mapper;
 import org.talend.sdk.component.runtime.output.Processor;
+import org.talend.sdk.component.spi.component.ComponentExtension;
 
-public class BeamDiExtension extends BeamComponentExtension {
+public class BeamDiExtension implements ComponentExtension {
+
+    private BeamComponentExtension delegate;
 
     @Override
     public int priority() {
-        return super.priority() - 1;
+        return delegate.priority() - 1;
     }
 
     @Override
-    public <T> T convert(final ComponentInstance instance, final Class<T> component) {
+    public boolean isActive() {
+        try {
+            delegate = new BeamComponentExtension() {
+
+                @Override
+                public <T> T convert(final ComponentInstance instance, final Class<T> component) {
+                    return doConvert(instance, component);
+                }
+            };
+        } catch (final NoClassDefFoundError | RuntimeException e) {
+            return false;
+        }
+        return delegate.isActive();
+    }
+
+    private <T> T doConvert(final ComponentInstance instance, final Class<T> component) {
         if (Mapper.class == component) {
             final org.apache.beam.sdk.transforms.PTransform<org.apache.beam.sdk.values.PBegin, org.apache.beam.sdk.values.PCollection<?>> begin =
                     (org.apache.beam.sdk.transforms.PTransform<org.apache.beam.sdk.values.PBegin, org.apache.beam.sdk.values.PCollection<?>>) instance
@@ -43,5 +65,35 @@ public class BeamDiExtension extends BeamComponentExtension {
             return component.cast(new QueueOutput(instance.plugin(), instance.family(), instance.name(), transform));
         }
         throw new IllegalArgumentException("unsupported " + component + " by " + getClass());
+    }
+
+    @Override
+    public <T> T unwrap(final Class<T> type, final Object... args) {
+        return delegate.unwrap(type, args);
+    }
+
+    @Override
+    public Collection<ClassFileTransformer> getTransformers() {
+        return delegate.getTransformers();
+    }
+
+    @Override
+    public void onComponent(final ComponentContext context) {
+        delegate.onComponent(context);
+    }
+
+    @Override
+    public boolean supports(final Class<?> componentType) {
+        return delegate.supports(componentType);
+    }
+
+    @Override
+    public Map<Class<?>, Object> getExtensionServices(final String plugin) {
+        return delegate.getExtensionServices(plugin);
+    }
+
+    @Override
+    public <T> T convert(final ComponentInstance instance, final Class<T> component) {
+        return delegate.convert(instance, component);
     }
 }
