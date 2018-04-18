188f8e8a15c20f287204e39c7dbfbc1ce67b1e2f
Dmytro Chmyga <dmytro.chmyga@synapse.com>
2018-04-18 14:27:20 +0300

    feat: additional dependencies for beam components
    * Add sample-parent module with sample-beam submodule

---
+++ sample-parent/sample-beam/src/main/java/org/talend/sdk/component/sample/beam/BeamSampleSource.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.sample.beam;
+
+import java.io.Serializable;
+
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+import org.talend.sdk.component.api.component.Icon;
+import org.talend.sdk.component.api.component.Version;
+import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.input.PartitionMapper;
+import org.talend.sdk.component.api.meta.Documentation;
+
+@Icon(Icon.IconType.DB_INPUT)
+@Version(1)
+@PartitionMapper(family = "beamsample", name = "Input")
+@Documentation("Sample beam input component")
+public class BeamSampleSource extends PTransform<PBegin, PCollection<String>> {
+
+    private final Configuration configuration;
+
+    public BeamSampleSource(@Option("configuration") final Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public PCollection<String> expand(final PBegin input) {
+        return input.apply(Create.of((Void) null)).apply(ParDo.of(new DoFn<Void, String>() {
+
+            @ProcessElement
+            public void processElement(final ProcessContext context) throws Exception {
+                context.output(configuration.getValue());
+            }
+        }));
+    }
+
+    public static class Configuration implements Serializable {
+
+        private final String value;
+
+        public Configuration(final String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+    }
+}
