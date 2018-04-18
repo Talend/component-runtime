64ffe34f147c8ee6bcabf559ae831d77df8aa3ea
Dmytro Chmyga <dmytro.chmyga@synapse.com>
2018-04-18 15:30:31 +0300

    feat: additional dependencies for beam components
    * Fix surefire plugin version

--- component-server/src/test/java/org/talend/sdk/component/server/test/ComponentClient.java
+++ component-server/src/test/java/org/talend/sdk/component/server/test/ComponentClient.java
@@ -52,7 +52,7 @@ public class ComponentClient {
     public String getJdbcId() {
         return getComponentId("jdbc", "input");
     }
-    
+
     public String getBeamSampleId() {
         return getComponentId("beamsample", "Input");
     }
