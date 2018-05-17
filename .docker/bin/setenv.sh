e478e14a33691b9e883fbad2db6842b8cc92c297
Anas KHABALI <akhabali@talend.com>
2018-05-17 18:22:14 +0100

    make brave.reporter.type default to log

--- .docker/bin/setenv.sh
+++ .docker/bin/setenv.sh
@@ -25,3 +25,4 @@ export MEECROWAVE_PID=$MEECROWAVE_BASE/conf/server.pid
 export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
 export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.exit-on-destroy=true"
 export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dlog4j.configurationFile="$MEECROWAVE_BASE"/conf/log4j2-"$TALEND_COMPONENT_LOG4J2_PROFILE".xml"
+export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.server.monitoring.brave.reporter.type=log" # make this an env ?
