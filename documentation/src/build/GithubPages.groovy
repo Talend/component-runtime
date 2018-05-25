5cca4c1309a880199dc04934b70dffa4cf182777
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-05-25 09:09:38 +0200

    fixing site deployment/indexation scripts after js-search drop

--- documentation/src/build/GithubPages.groovy
+++ documentation/src/build/GithubPages.groovy
@@ -24,7 +24,7 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 
 import static java.util.Collections.singleton
 
-if (!new File(project.build.directory, "${project.build.finalName}/_/js/js-search.min.js").exists()) {
+if (!new File(project.build.directory, "${project.build.finalName}/_/js/instantsearch.min.js").exists()) {
     log.info('Not yet in deploy phase, should generate the site before')
     return
 }
