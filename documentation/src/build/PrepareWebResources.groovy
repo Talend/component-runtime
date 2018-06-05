062a640451b7eeba9c3fb7a65731f3df020e9f29
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-05 18:44:35 +0200

    few doc setup fixes and cleanup

--- documentation/src/build/PrepareWebResources.groovy
+++ documentation/src/build/PrepareWebResources.groovy
@@ -15,14 +15,6 @@
  */
 package org.talend.sdk.component.build
 
-import org.asciidoctor.Asciidoctor
-import org.asciidoctor.AttributesBuilder
-import org.asciidoctor.OptionsBuilder
-import org.asciidoctor.SafeMode
-import org.jsoup.Jsoup
-
-def adoc = Asciidoctor.Factory.create()
-
 def copyJsResource = { source, output ->
     def target = new File(project.build.directory, "${project.build.finalName}/_/${output}")
     target.parentFile.mkdirs()
@@ -43,54 +35,6 @@ copyJsResource(
 copyJsResource(
     new File(project.basedir, 'src/main/frontend/node_modules/instantsearch.js/dist/instantsearch.min.js').toURI().toURL(),
     'js/instantsearch.min.js')
-
-// temp antora patch to add the branch
-def antoraLoadAdoc = new File(project.basedir, 'src/main/frontend/node_modules/@antora/asciidoc-loader/lib/load-asciidoc.js')
-if (!antoraLoadAdoc.text.contains('docbranch')) {
-    antoraLoadAdoc.text = antoraLoadAdoc.text.replace(
-            'const intrinsicAttrs = {',
-            'const intrinsicAttrs = {docbranch: (file.origin || (file.src || {}).origin || { branch: \'unknown\' }).branch, docversion: (file.src || {}).version,')
-}
-
-// populate index
-class Document {
-    String title
-    String content
-    String link
-}
-
-def readTitle = { file ->
-    def firstLine = file.readLines()[0]
-    if (firstLine.startsWith('=')) {
-        return firstLine.substring(firstLine.lastIndexOf('=') + 1).trim()
-    }
-    return file.name.replace('.adoc', '').replace('-', ' ')
-}
-def readContent = { file ->
-    file.readLines()
-            .findAll { !it.startsWith(':') && !it.startsWith('=') && !it.trim().isEmpty() }
-            .join(' ')
-            .replace('`', '')
-}
-def toText = { content ->
-    def html = adoc.render(content, OptionsBuilder.options()
-            .safe(SafeMode.UNSAFE)
-            .backend('html5')
-            .headerFooter(true)
-            .attributes(AttributesBuilder.attributes()
-            .attribute('hide-uri-scheme')
-            .attribute('idprefix')
-            .attribute('idseparator')
-            .attribute('partialsdir', new File(project.basedir, 'src/main/antora/modules/ROOT/pages/_partials').absolutePath)
-            .attribute('imagesdir', new File(project.basedir, 'src/main/antora/modules/ROOT/assets/images').absolutePath)
-            .attribute('project_version', project.properties['versions.release'])
-            .attribute('docversion', project.properties['versions.release'])
-            .attribute('git_branch', project.properties['git.branch'])
-            .attribute('deploymentRoot', 'https://talend.github.io/component-runtime')))
-    Jsoup.parse(html).body().text()
-        .replaceAll("^Version: ${project.properties['versions.release']}", '')
-        .replaceAll('Last updated [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} CEST$', '')
-}
 
 // otherwise gh-pages ignore the _ folders
 new File(project.build.directory, "${project.build.finalName}/.nojekyll").text = ''
