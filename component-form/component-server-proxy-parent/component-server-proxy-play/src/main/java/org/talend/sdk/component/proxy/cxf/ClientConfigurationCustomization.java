b141ad42a85387fe5e53f6c2031c43d8a5fb69e9
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-05 10:08:44 +0200

    simpler config of the client bus in the proxy

--- component-form/component-server-proxy-parent/component-server-proxy-play/src/main/java/org/talend/sdk/component/proxy/cxf/ClientConfigurationCustomization.java
+++ component-form/component-server-proxy-parent/component-server-proxy-play/src/main/java/org/talend/sdk/component/proxy/cxf/ClientConfigurationCustomization.java
@@ -15,100 +15,48 @@
  */
 package org.talend.sdk.component.proxy.cxf;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import static java.util.Collections.emptyMap;
-
-import java.io.Serializable;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import javax.annotation.Priority;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.inject.spi.ProcessAnnotatedType;
-import javax.enterprise.util.AnnotationLiteral;
-import javax.inject.Inject;
-import javax.interceptor.AroundInvoke;
-import javax.interceptor.Interceptor;
-import javax.interceptor.InterceptorBinding;
-import javax.interceptor.InvocationContext;
-import javax.ws.rs.client.Client;
 
 import org.apache.cxf.Bus;
-import org.apache.cxf.BusFactory;
-import org.apache.cxf.bus.CXFBusFactory;
 import org.apache.cxf.transport.http.asyncclient.AsyncHTTPConduitFactory;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.talend.sdk.component.proxy.config.ProxyConfiguration;
-import org.talend.sdk.component.proxy.service.client.ClientProducer;
 
 public class ClientConfigurationCustomization implements Extension {
 
-    void onProducer(@Observes final ProcessAnnotatedType<ClientProducer> producer) {
-        producer
-                .configureAnnotatedType()
-                .filterMethods(m -> m.getJavaMember().getReturnType() == Client.class
-                        && m.getJavaMember().getName().equals("client"))
-                .forEach(m -> m.add(new AnnotationLiteral<CxfCustomization>() {
-                }));
-    }
-
-    @InterceptorBinding
-    @Target({ TYPE, METHOD })
-    @Retention(RUNTIME)
-    public @interface CxfCustomization {
-    }
-
-    @Interceptor
-    @CxfCustomization
-    @Priority(Interceptor.Priority.APPLICATION)
-    public static class CxfCustomizationInterceptor implements Serializable {
-
-        @Inject
-        private ProxyConfiguration configuration;
-
-        @AroundInvoke
-        public Object around(final InvocationContext context) throws Exception {
-            final Bus originalBus = BusFactory.getDefaultBus(false);
-            BusFactory.setDefaultBus(new CXFBusFactory().createBus(emptyMap(), createClientConfiguration()));
-            try {
-                return context.proceed();
-            } finally {
-                BusFactory.getAndSetThreadDefaultBus(originalBus);
-            }
-        }
-
-        private Map<String, Object> createClientConfiguration() {
-            final Map<String, Object> config = new HashMap<>();
-            final Config configSource = ConfigProvider.getConfig();
-            Stream
-                    .of(AsyncHTTPConduitFactory.class.getDeclaredFields())
-                    .filter(f -> Modifier.isStatic(f.getModifiers()) && f.getType() == String.class)
-                    .map(f -> {
-                        try {
-                            if (!f.isAccessible()) {
-                                f.setAccessible(true);
-                            }
-                            return f.get(null);
-                        } catch (final IllegalAccessException e) {
-                            return null;
+    void customizeBus(@Observes final AfterDeploymentValidation afterDeploymentValidation,
+            final BeanManager beanManager) {
+        final Bus bus = Bus.class.cast(beanManager.getReference(beanManager.resolve(beanManager.getBeans(Bus.class)),
+                Bus.class, beanManager.createCreationalContext(null)));
+        final ProxyConfiguration configuration = ProxyConfiguration.class
+                .cast(beanManager.getReference(beanManager.resolve(beanManager.getBeans(ProxyConfiguration.class)),
+                        ProxyConfiguration.class, beanManager.createCreationalContext(null)));
+        final Config configSource = ConfigProvider.getConfig();
+        Stream
+                .of(AsyncHTTPConduitFactory.class.getDeclaredFields())
+                .filter(f -> Modifier.isStatic(f.getModifiers()) && f.getType() == String.class)
+                .map(f -> {
+                    try {
+                        if (!f.isAccessible()) {
+                            f.setAccessible(true);
                         }
-                    })
-                    .filter(Objects::nonNull)
-                    .map(String::valueOf)
-                    .filter(key -> key.startsWith("org.apache.cxf.transport.http.async"))
-                    .forEach(key -> configSource
-                            .getOptionalValue(configuration.getPrefix() + key, String.class)
-                            .ifPresent(val -> config.put(key, val)));
-            return config;
-        }
+                        return f.get(null);
+                    } catch (final IllegalAccessException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .map(String::valueOf)
+                .filter(key -> key.startsWith("org.apache.cxf.transport.http.async"))
+                .forEach(key -> configSource.getOptionalValue(configuration.getPrefix() + key, String.class).ifPresent(
+                        val -> bus.setProperty(key, val)));
     }
 }
