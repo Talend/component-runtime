80917512f7422ac62512a516a68d803b52c17547
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-27 11:04:40 +0200

    fixing encrypted file location

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x"
     echo "Skipping deployment setup since we are building a pull request"
 else
     mkdir -p ~/.build/
-    openssl aes-256-cbc -K $encrypted_f13b410addf6_key -iv $encrypted_f13b410addf6_iv -in encrypted.tar.gz.enc -out encrypted.tar.gz -d
+    openssl aes-256-cbc -K $encrypted_f13b410addf6_key -iv $encrypted_f13b410addf6_iv -in .travis/encrypted.tar.gz.enc -out encrypted.tar.gz -d
     tar xvf ~/.gpg/gpg.tar.gz -C ~/.build/
     mv ~/.build/docker ~/.docker/
     mv ~/.build/gpg ~/.gpg/
