1b2ce6f5cda65cbdda8a8a9c3f96c1e07a5004bb
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-27 11:31:15 +0200

    fixing setup_keys.sh

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -3,9 +3,9 @@
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x" ]; then
     echo "Skipping deployment setup since we are building a pull request"
 else
-    mkdir -p ~/.build/
-    openssl aes-256-cbc -K $encrypted_f13b410addf6_key -iv $encrypted_f13b410addf6_iv -in .travis/encrypted.tar.gz.enc -out encrypted.tar.gz -d
-    tar xvf ~/.gpg/gpg.tar.gz -C ~/.build/
+    mkdir -p ~/.build/ ~/.build_source/
+    openssl aes-256-cbc -K $encrypted_f13b410addf6_key -iv $encrypted_f13b410addf6_iv -in .travis/encrypted.tar.gz.enc -out ~/.build_source/encrypted.tar.gz -d
+    tar xvf ~/.build_source/encrypted.tar.gz -C ~/.build/
     mv ~/.build/docker ~/.docker/
     mv ~/.build/gpg ~/.gpg/
 fi
