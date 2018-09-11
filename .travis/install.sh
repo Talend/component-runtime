9841471dbf38c790e7c2f9c5bd1ee9e72a4f03cd
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-09-11 08:52:41 +0200

    only openjdk needs openjfx package, dont force all steps to use that

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,3 @@ date
 
 # travis helper
 mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-output:1.0.0 -DoutputDirectory=/tmp
-
-# openjdk requires this to get javafx (intellij plugin) on travis
-sudo add-apt-repository ppa:tj/java-for-14.04 && \
-sudo apt-get update && \
-sudo apt-get install openjfx
