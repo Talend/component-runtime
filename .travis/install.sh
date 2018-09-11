ee83aa980c71017ba9c9206743662601ac48ec33
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-09-11 08:48:54 +0200

    doc + adding sudo in travis.yml

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,6 +6,6 @@ date
 mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-output:1.0.0 -DoutputDirectory=/tmp
 
 # openjdk requires this to get javafx (intellij plugin) on travis
-add-apt-repository ppa:tj/java-for-14.04 && \
-apt-get update && \
-apt-get install openjfx
+sudo add-apt-repository ppa:tj/java-for-14.04 && \
+sudo apt-get update && \
+sudo apt-get install openjfx
