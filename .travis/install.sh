bd28c016305b75731217aa9d703cf0b6a01ead54
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-09-11 08:43:54 +0200

    trying to install openjfx to get openjdk build passing

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,3 +4,8 @@ date
 
 # travis helper
 mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-output:1.0.0 -DoutputDirectory=/tmp
+
+# openjdk requires this to get javafx (intellij plugin) on travis
+add-apt-repository ppa:tj/java-for-14.04 && \
+apt-get update && \
+apt-get install openjfx
