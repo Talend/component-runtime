368b07a3f3be4b9c48d83213974dd34ca1d375c2
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-01-10 10:26:56 +0100

    TCOMP-1254 use spotless instead of formatter plugin

--- component-tools/src/test/java/org/talend/sdk/component/tools/ComponentValidatorTest.java
+++ component-tools/src/test/java/org/talend/sdk/component/tools/ComponentValidatorTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.talend.sdk.component.tools;
 
 import static java.lang.annotation.ElementType.METHOD;
