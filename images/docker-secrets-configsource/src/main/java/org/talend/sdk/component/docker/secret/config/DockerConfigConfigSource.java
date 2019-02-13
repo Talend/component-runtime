5a15d60626427a63febc7e75f0e2dacd5a8f8266
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-02-13 15:28:46 +0100

    TCOMP-1303 docker configs/secrets support in docker images

---
+++ images/docker-secrets-configsource/src/main/java/org/talend/sdk/component/docker/secret/config/DockerConfigConfigSource.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.docker.secret.config;
+
+import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.toMap;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+public class DockerConfigConfigSource implements ConfigSource {
+
+    private final Map<String, String> entries;
+
+    private final int ordinal;
+
+    public DockerConfigConfigSource() {
+        this(System.getProperty("talend.docker.configs.base", "/"),
+                System.getProperty("talend.docker.configs.prefix", "talend."),
+                Integer.getInteger("talend.docker.configs.ordinal", 100));
+    }
+
+    public DockerConfigConfigSource(final String base, final String prefix, final int ordinal) {
+        this.ordinal = ordinal;
+
+        final Path from = Paths.get(base);
+        if (!Files.exists(from)) {
+            entries = emptyMap();
+        } else {
+            try {
+                entries = Files
+                        .list(from)
+                        .filter(it -> it.getFileName().toString().startsWith(prefix))
+                        .collect(toMap(it -> it.getFileName().toString(), it -> {
+                            try {
+                                return new String(Files.readAllBytes(it), StandardCharsets.UTF_8);
+                            } catch (final IOException e) {
+                                throw new IllegalStateException(e);
+                            }
+                        }));
+            } catch (final IOException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    @Override
+    public int getOrdinal() {
+        return ordinal;
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return entries;
+    }
+
+    @Override
+    public String getValue(final String propertyName) {
+        return entries.get(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return "docker-configs";
+    }
+}
