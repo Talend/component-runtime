9bde24190b3dbf3046737210c3a1f0d77de5dc6b
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-05-10 10:02:43 +0200

    fixing travis jetbrains password var

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,6 +9,3 @@ mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-outp
 
 # ensure default settings.xml works contextually without specifying it
 cp -v $HOME/build/Talend/component-runtime/.travis/settings.xml $HOME/.m2/settings.xml
-
-# jib in 1.2.0 decrypts all servers so we must provide a settings-security.xml - not used in our travis build
-echo "<settingsSecurity><master>$(mvn --encrypt-password notused)</master></settingsSecurity>" > $HOME/.m2/settings-security.xml
