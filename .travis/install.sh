deea2c8fe16e162cc320ea4640892e1c60a43d9d
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-06-10 15:28:57 +0200

    timeout for the doc
    trying to simplify travis.yml with sdkman
    
    small refactor of global travis vars
    
    escape bash test in travis.yml
    
    test sdkman init script before installing
    
    fix travis sdkman test
    
    install sdkman through install.sh
    
    installing maven in travis
    
    sdkman use in travis.yml
    
    source install.sh
    
    fake dir to get maven-travis-output jar and avoid ./pom.xml resolution (travis)
    
    a few more travis_wait

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,26 @@ date
 
 mkdir -p $HOME/.m2/
 
+# ensure sdkman is installed
+java_version=${JAVA_DISTRO:-$JAVA8_DISTRO}
+maven_version=3.6.0
+
+# rm -Rf $HOME/.sdkman/
+[ ! -f $HOME/.sdkman/bin/sdkman-init.sh ] && curl -s https://get.sdkman.io | bash && mkdir -p ~/.sdkman/etc/
+source $HOME/.sdkman/bin/sdkman-init.sh
+echo sdkman_auto_answer=true > ~/.sdkman/etc/config
+source $HOME/.sdkman/bin/sdkman-init.sh
+sdk selfupdate force && \
+    sdk install java $java_version && \
+    sdk install maven $maven_version
+
+# log mvn+java versions
+mvn -version
+
 # travis helper
-travis_wait 50 mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-output:1.0.0 -DoutputDirectory=/tmp
+mkdir -p /tmp/dep && cd /tmp/dep &&
+  travis_wait 50 mvn -Ptravis dependency:copy -Dartifact=com.github.rmannibucau:maven-travis-output:1.0.0 -DoutputDirectory=/tmp &&
+cd -
 
 # ensure default settings.xml works contextually without specifying it
 cp -v $HOME/build/Talend/component-runtime/.travis/settings.xml $HOME/.m2/settings.xml
