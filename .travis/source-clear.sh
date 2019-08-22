914aae04277ce20b0a447d337f096e71817ac90d
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-08-22 10:35:43 +0200

    adding sourceclear on travis, fixing repo url in pom.xml and forcing commons-io version

--- .travis/source-clear.sh
+++ .travis/source-clear.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-curl -sSL https://download.sourceclear.com/ci.sh | bash
+curl -sSL https://download.sourceclear.com/ci.sh | CACHE_DIR="$HOME/build/Talend/component-runtime/.sourceclear" bash
