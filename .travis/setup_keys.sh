36c836841d094f0a3d08a55adc280e94863307c2
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-09-03 14:00:54 +0200

    Adding github actions setup

--- .travis/setup_keys.sh
+++ .travis/setup_keys.sh
@@ -1,11 +1,30 @@
 #! /bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_03e441b90173_key" = "x" ]; then
+function createGpgDirIfNeeded() {
+    [ ! -d "$1" ] && \
+        mkdir -p "$1" && \
+        chown -R "$USER:$(id -gn)" "$1" && \
+        chmod 700 "$1"
+}
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "x$encrypted_f13b410addf6_key" = "x" ]; then
     echo "Skipping deployment setup since we are building a pull request"
 else
-    mkdir -p ~/.build/ ~/.build_source/
-    openssl aes-256-cbc -K $encrypted_f13b410addf6_key -iv $encrypted_f13b410addf6_iv -in .travis/encrypted.tar.gz.enc -out ~/.build_source/encrypted.tar.gz -d
-    tar xvf ~/.build_source/encrypted.tar.gz -C ~/.build/
-    mv ~/.build/docker ~/.docker/
-    mv ~/.build/gpg ~/.gpg/
+    mkdir -p .build .build_source
+
+    openssl aes-256-cbc \
+        -K "$encrypted_f13b410addf6_key" \
+        -iv "$encrypted_f13b410addf6_iv" \
+        -in .travis/encrypted.tar.gz.enc \
+        -out .build_source/encrypted.tar.gz -d
+
+    tar xf .build_source/encrypted.tar.gz -C .build
+
+    createGpgDirIfNeeded "$GPG_DIR"
+
+    cp -v .build/gpg/travis.* "$GPG_DIR/"
+
+    if [ "$GPG_IMPORT_PRIVATE_KEY" = "true" ]; then
+        gpg --import --no-tty --batch --yes "$GPG_DIR/travis.priv.bin"
+    fi
 fi
--- .travis/source-clear.sh
+++ .travis/source-clear.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-curl -sSL https://download.sourceclear.com/ci.sh | CACHE_DIR="$HOME/build/Talend/component-runtime/.sourceclear" bash
+cache_dir="${CACHE_DIR:-$HOME/build/Talend/component-runtime}" &&
+  mkdir -p "$cache_dir" &&
+  curl -sSL https://download.sourceclear.com/ci.sh | CACHE_DIR="${cache_dir}/.sourceclear" bash
