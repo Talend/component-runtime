84520923262b4191aa749b71398ec9862266a622
undx <egallois@talend.com>
2020-02-12 17:09:27 +0100

    TCOMP-1636 - fix intellij-plugin release deployment

--- talend-component-kit-intellij-plugin/src/build/IntelliJPublish.groovy
+++ talend-component-kit-intellij-plugin/src/build/IntelliJPublish.groovy
@@ -17,7 +17,7 @@ package org.talend.sdk.component.build
 
 import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest
 import org.apache.maven.settings.crypto.SettingsDecrypter
-import org.jetbrains.intellij.pluginRepository.PluginRepositoryInstance
+import org.jetbrains.intellij.pluginRepository.PluginRepositoryFactory
 
 //
 // Helper script to publish a plugin to jetbrains repository using a settings.xml server
@@ -51,11 +51,11 @@ def decryptedServer = session.container.lookup(SettingsDecrypter).decrypt(new De
 server = decryptedServer.server != null ? decryptedServer.server : server
 
 int pluginId = Integer.parseInt(project.properties['idea.plugin.id'].trim())
-def repositoryInstance = new PluginRepositoryInstance(project.properties['idea.intellij.public.url'], server.getPassword())
+def repositoryInstance = PluginRepositoryFactory.create(project.properties['idea.intellij.public.url'], server.getPassword())
 project.properties['idea.intellij.channel'].trim().split(',').each { channel ->
     try {
-        repositoryInstance
-                .uploadPlugin(pluginId, pluginZip, channel == 'default' ? '' : channel)
+        repositoryInstance.uploader
+                .uploadPlugin(pluginId, pluginZip, channel == 'default' ? '' : channel, null)
     } catch (final Exception e) { // don't break the release for that, worse case upload it manually
         log.error(e.message, e)
     }
