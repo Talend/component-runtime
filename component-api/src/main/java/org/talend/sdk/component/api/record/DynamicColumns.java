786b6f8b53bbf9adf0f0c9db77bbcb886e8e6d72
undx <egallois@talend.com>
2020-06-15 17:19:02 +0200

    feat(TCOMP-1651): add @DynamicColumns
    - add documentation and string prefix for column identification

--- component-api/src/main/java/org/talend/sdk/component/api/record/DynamicColumns.java
+++ component-api/src/main/java/org/talend/sdk/component/api/record/DynamicColumns.java
@@ -1,18 +1,19 @@
-/*
+/**
  * Copyright (C) 2006-2020 Talend Inc. - www.talend.com
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.talend.sdk.component.api.record;
-
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -22,8 +23,12 @@ import java.lang.annotation.Target;
 
 import org.talend.sdk.component.api.meta.Documentation;
 
+@Documentation("Declare that the component is able to handle Studio Dynamic type. This is a Studio only feature."
+        + "The annotation has to be declared on components implementing  `@Producer` and `@ElementListener` methods.")
+
 @Target(TYPE)
 @Retention(RUNTIME)
 public @interface DynamicColumns {
 
+    final String DYNAMIC_COLUMN_PREFIX = "$$DYNAMIC_COLUMN";
 }
