f932dfa29242310d32baacf0ae4ec072ff7258b5
undx <egallois@talend.com>
2020-10-23 18:32:23 +0200

    fix(TCOMP-1788): change enumvalidation rules

---
+++ component-form/component-form-core/src/main/java/org/talend/sdk/component/form/internal/validation/JsonSchemaValidatorFactoryExt.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.form.internal.validation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.talend.sdk.component.form.internal.validation.spi.ext.EnumValidationWithDefaultValue;
+import org.talend.sdk.component.form.internal.validation.spi.ext.MaximumValidation;
+import org.talend.sdk.component.form.internal.validation.spi.ext.MinimumValidation;
+
+public class JsonSchemaValidatorFactoryExt extends org.apache.johnzon.jsonschema.JsonSchemaValidatorFactory {
+
+    @Override
+    public List<org.apache.johnzon.jsonschema.spi.ValidationExtension> createDefaultValidations() {
+        List validatons = super.createDefaultValidations()
+                .stream()
+                .filter(v -> !org.apache.johnzon.jsonschema.spi.builtin.EnumValidation.class.isInstance(v))
+                .filter(v -> !org.apache.johnzon.jsonschema.spi.builtin.MinimumValidation.class.isInstance(v))
+                .filter(v -> !org.apache.johnzon.jsonschema.spi.builtin.MaximumValidation.class.isInstance(v))
+                .collect(Collectors.toList());
+        validatons.add(new EnumValidationWithDefaultValue());
+        validatons.add(new MinimumValidation());
+        validatons.add(new MaximumValidation());
+
+        return validatons;
+    }
+}
