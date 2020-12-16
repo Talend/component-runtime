65031575e20ed859e9856f2c51cb56fd6bd2ad6f
undx <egallois@talend.com>
2020-12-16 14:27:26 +0100

    feat(TCOMP-1833): Upgrade spark to 2.4.7

--- component-runtime-testing/component-runtime-testing-spark/src/test/java/org/talend/sdk/component/runtime/testing/spark/SparkClusterRuleTest.java
+++ component-runtime-testing/component-runtime-testing-spark/src/test/java/org/talend/sdk/component/runtime/testing/spark/SparkClusterRuleTest.java
@@ -43,7 +43,7 @@ import lombok.NoArgsConstructor;
 public class SparkClusterRuleTest {
 
     @ClassRule
-    public static final SparkClusterRule SPARK = new SparkClusterRule("2.11", "2.4.0", 1);
+    public static final SparkClusterRule SPARK = new SparkClusterRule("2.11", "2.4.7", 1);
 
     @Rule
     public final TestName testName = new TestName();
