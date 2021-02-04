54e57ee534e7a2646c75bd17f5cff07f29f366f3
undx <egallois@talend.com>
2021-02-04 14:55:42 +0100

    feat(TCOMP-1849): proxify metrics

--- component-server-parent/component-server-vault-proxy/src/main/java/org/talend/sdk/component/runtime/server/vault/proxy/endpoint/jaxrs/RequestForwarder.java
+++ component-server-parent/component-server-vault-proxy/src/main/java/org/talend/sdk/component/runtime/server/vault/proxy/endpoint/jaxrs/RequestForwarder.java
@@ -65,8 +65,18 @@ public class RequestForwarder {
         return forward(null, null);
     }
 
+    public CompletionStage<Response> forward(final String removePath) {
+        return forward(null, null, removePath);
+    }
+
     public CompletionStage<Response> forward(final InputStream payload, final Function<Response, Response> customizer) {
-        WebTarget target = client.path(uriInfo.getPath());
+        return forward(payload, customizer, null);
+    }
+
+    public CompletionStage<Response> forward(final InputStream payload, final Function<Response, Response> customizer,
+            final String removePath) {
+        WebTarget target =
+                client.path(removePath != null ? uriInfo.getPath().replace(removePath, "") : uriInfo.getPath());
         for (final Map.Entry<String, List<String>> query : uriInfo.getQueryParameters().entrySet()) {
             target = target.queryParam(query.getKey(), query.getValue().toArray(emptyObjectsArray));
         }
