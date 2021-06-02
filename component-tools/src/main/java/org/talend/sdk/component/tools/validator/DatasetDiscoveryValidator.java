849c25a919f03abd07da8252f028368b915b275c
rlecomte-talend <rlecomte@talend.com>
2021-06-02 09:35:05 +0200

    feat(TCOMP-1917) : DatasetDiscovery annotation

--- component-tools/src/main/java/org/talend/sdk/component/tools/validator/DatasetDiscoveryValidator.java
+++ component-tools/src/main/java/org/talend/sdk/component/tools/validator/DatasetDiscoveryValidator.java
@@ -1,4 +1,24 @@
+/**
+ * Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.talend.sdk.component.tools.validator;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 import java.util.List;
 import java.util.Map;
@@ -13,23 +33,20 @@ import org.talend.sdk.component.runtime.manager.reflect.ParameterModelService;
 import org.talend.sdk.component.runtime.manager.reflect.parameterenricher.BaseParameterEnricher;
 import org.talend.sdk.component.runtime.manager.service.LocalConfigurationService;
 
-import lombok.RequiredArgsConstructor;
-
-import static java.util.Collections.emptyList;
-import static java.util.Optional.ofNullable;
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
-
-@RequiredArgsConstructor
 public class DatasetDiscoveryValidator implements Validator {
 
     private final Validators.ValidatorHelper helper;
 
+    public DatasetDiscoveryValidator(final Validators.ValidatorHelper helper) {
+        this.helper = helper;
+    }
+
     @Override
-    public Stream<String> validate(AnnotationFinder finder, List<Class<?>> components) {
+    public Stream<String> validate(final AnnotationFinder finder, final List<Class<?>> components) {
         final List<Class<?>> datasetDiscoveryClasses = finder.findAnnotatedClasses(DatasetDiscovery.class);
-        final Map<Class<?>, String> datasetDiscoveries =
-                datasetDiscoveryClasses.stream().collect(toMap(identity(), d -> d.getAnnotation(DatasetDiscovery.class).value()));
+        final Map<Class<?>, String> datasetDiscoveries = datasetDiscoveryClasses
+                .stream()
+                .collect(toMap(identity(), d -> d.getAnnotation(DatasetDiscovery.class).value()));
 
         final Stream<String> duplicated = DatasetValidator.duplicatedDataset(datasetDiscoveries.values());
 
@@ -51,10 +68,11 @@ public class DatasetDiscoveryValidator implements Validator {
                 .sorted();
 
         // A datasetDiscovery must implement interface DatasetDiscoveryConfiguration
-        final Stream<String> implementationError =  datasetDiscoveryClasses
+        final Stream<String> implementationError = datasetDiscoveryClasses
                 .stream()
                 .filter((Class<?> ds) -> !(ds.isAssignableFrom(DatasetDiscoveryConfiguration.class)))
-                .map((Class<?> ds) -> "Class " + ds.getName() + " must implement " + DatasetDiscoveryConfiguration.class)
+                .map((Class<?> ds) -> "Class " + ds.getName() + " must implement "
+                        + DatasetDiscoveryConfiguration.class)
                 .sorted();
 
         return Stream
@@ -63,12 +81,15 @@ public class DatasetDiscoveryValidator implements Validator {
                 .orElseGet(Stream::empty);
     }
 
-    private String findDatasetDiscoveryWithoutDataStore(final Class<?> ds, final BaseParameterEnricher.Context context) {
+    private String findDatasetDiscoveryWithoutDataStore(final Class<?> ds,
+            final BaseParameterEnricher.Context context) {
         final List<ParameterMeta> dataset = helper
                 .getParameterModelService()
-                .buildParameterMetas(Stream.of(new ParameterModelService.Param(ds, ds.getAnnotations(), "datasetDiscovery")), ds,
+                .buildParameterMetas(
+                        Stream.of(new ParameterModelService.Param(ds, ds.getAnnotations(), "datasetDiscovery")), ds,
                         ofNullable(ds.getPackage()).map(Package::getName).orElse(""), true, context);
-        if (DatasetValidator.flatten(dataset)
+        if (DatasetValidator
+                .flatten(dataset)
                 .noneMatch(prop -> "datastore".equals(prop.getMetadata().get("tcomp::configurationtype::type")))) {
             return "The datasetDiscovery " + ds.getName()
                     + " is missing a datastore reference in its configuration (see @DataStore)";
