23f7d2a2a5fdf77bfec81113fec8aeaebd91e46a
Christophe Le Saec <clesaec@talend.com>
2022-02-07 14:41:11 +0100

    TCOMP-2080 : filter ENTRIES_ORDER_PROP

--- component-runtime-beam/src/main/java/org/talend/sdk/component/runtime/beam/spi/record/AvroSchemaBuilder.java
+++ component-runtime-beam/src/main/java/org/talend/sdk/component/runtime/beam/spi/record/AvroSchemaBuilder.java
@@ -339,8 +339,11 @@ public class AvroSchemaBuilder implements Schema.Builder {
                     .createRecord(SchemaIdGenerator.generateRecordName(avroFields), null, "talend.component.schema",
                             false);
             record.setFields(avroFields);
-            this.props.forEach(record::addProp);
             record.addProp(ENTRIES_ORDER_PROP, fields.stream().map(e -> e.getName()).collect(Collectors.joining(",")));
+            this.props.entrySet()
+                    .stream()
+                    .filter((Map.Entry<String, String> e) -> !ENTRIES_ORDER_PROP.equals(e.getKey()))
+                    .forEach((Map.Entry<String, String> e) -> record.addProp(e.getKey(), e.getValue()));
             return new AvroSchema(record);
         case ARRAY:
             if (elementSchema == null) {
