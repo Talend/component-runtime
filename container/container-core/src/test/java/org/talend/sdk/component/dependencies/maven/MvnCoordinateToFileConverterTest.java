3e3854dd1380ac2052f0c173794434321aa82d1e
undx <egallois@talend.com>
2022-03-02 15:37:28 +0100

    chore(TCOMP-2092): add changelog and formatting

--- container/container-core/src/test/java/org/talend/sdk/component/dependencies/maven/MvnCoordinateToFileConverterTest.java
+++ container/container-core/src/test/java/org/talend/sdk/component/dependencies/maven/MvnCoordinateToFileConverterTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.talend.sdk.component.dependencies.maven;
 
 import org.junit.jupiter.api.Assertions;
@@ -9,10 +24,15 @@ import org.junit.jupiter.api.Test;
 class MvnCoordinateToFileConverterTest {
 
     public static final String GROUP_ID = "org.talend.sdk.component";
+
     public static final String ARTIFACT_ID = "container-core";
+
     public static final String TYPE = "jar";
+
     public static final String VERSION = "1.42.0";
+
     public static final String CLASSIFIER = "osx-aarch_64";
+
     public static final String SCOPE = "compile";
 
     @Test
@@ -93,8 +113,7 @@ class MvnCoordinateToFileConverterTest {
         final MvnCoordinateToFileConverter converter = new MvnCoordinateToFileConverter();
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> converter
-                        .toArtifact(GROUP_ID + ":" + ARTIFACT_ID)
-        );
+                        .toArtifact(GROUP_ID + ":" + ARTIFACT_ID));
     }
 
     @Test
