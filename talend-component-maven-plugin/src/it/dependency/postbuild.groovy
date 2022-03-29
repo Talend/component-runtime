33268304c30e99cf61c3f81d494e9cbfbf9c2cd9
C​⁠‌​⁠⁠‌​﻿​⁠‍‌‌​​‍‌yprien Q​⁠‌​⁠⁠‌​﻿​⁠‍‌‌​​‍‌uilici <cquilici@talend.com>
2022-03-29 14:16:38 +0200

    fix(TDI-2102): Fix cloud components image build (#556)
    * fix(TDI-2102): Fix cloud components image build
    
    * feat(TDI-2102): Add test for prepare-repository mojo

---
+++ talend-component-maven-plugin/src/it/dependency/postbuild.groovy
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import static groovy.io.FileType.FILES
+import static java.nio.file.Files.exists
+
+/**
+ * Checks that the IT on mojo prepare-repository ran smoothly
+ */
+static def checkPrepareRepositoryMojo (final File basedir, final String componentVersion, final String componentArtifactId) {
+    final Path builtM2 = Paths.get(basedir.toString())
+            .resolve('target/talend-component-kit')
+
+    if (!exists(builtM2)) {
+        throw new FileNotFoundException("Could not find built m2 ${builtM2}")
+    }
+
+    final Path componentJar = builtM2.resolve(
+            "maven/org/talend/components/${componentArtifactId}/${componentVersion}/${componentArtifactId}-${componentVersion}.jar"
+    )
+    if (!exists(componentJar)) {
+        throw new FileNotFoundException("Could not find component JAR ${componentJar}")
+    }
+
+    final List<String> allJars = []
+    new File(builtM2.toAbsolutePath().toString())
+            .traverse(type: FILES, nameFilter: ~/.*\.jar$/) { it ->
+                allJars.add(it.getPath())
+            }
+
+    if (allJars.size() <= 1) {
+        throw new IllegalStateException('Only the component jar was found with no dependency')
+    }
+}
+
+checkPrepareRepositoryMojo(basedir, componentVersion, componentArtifactId)
