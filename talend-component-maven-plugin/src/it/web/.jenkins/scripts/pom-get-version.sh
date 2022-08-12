938e9a4f2b9e7bb31a9cfeb839d5265342b34bed
acatoire <acatoire@talend.com>
2022-08-12 12:48:55 +0200

    Read pom version

---
+++ talend-component-maven-plugin/src/it/web/.jenkins/scripts/pom-get-version.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+#  Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# Change pom versions
+main() (
+  pom_path="${1:?Missing connector path}"
+
+  # shellcheck disable=SC2016
+  mvn -q \
+      -f "${pom_path}" \
+      -Dexec.executable=echo \
+      -Dexec.args='${project.version}' \
+      --non-recursive \
+      exec:exec
+)
+
+main "$@"
