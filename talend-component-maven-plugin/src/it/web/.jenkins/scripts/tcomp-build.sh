dec2c857e1559a8f98a98c9aa360babd12f2d56f
acatoire <acatoire@talend.com>
2022-08-16 10:45:56 +0200

    add tcomp build

---
+++ talend-component-maven-plugin/src/it/web/.jenkins/scripts/tcomp-build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+#  Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# $1: 'folder_to_build'
+
+
+MAVEN_FAST_CMD="mvn \
+                -Dspotless.apply.skip=true \
+                -Dspotbugs.skip=true \
+                -Dcheckstyle.skip=true -Drat.skip=true \
+                -DskipTests \
+                -Dmaven.javadoc.skip=true \
+                -Dinvoker.skip=true \
+                -Dmaven.artifact.threads=25"
+
+# check parameters
+[ -z ${1+x} ] && usage "Parameter 'folder_to_build'"
+
+folder_to_build=${1}
+
+main() (
+  echo "##############################################"
+  echo "Maven fast build"
+  echo "##############################################"
+
+  ${MAVEN_FAST_CMD} -f "${folder_to_build}"
+)
+
+function usage(){
+  echo "Build the given folder in fast mode"
+  echo "Usage : $0 <folder_to_build>"
+  echo
+  echo "$1 is needed."
+  echo
+  exit 1
+}
+
+main "$@"
