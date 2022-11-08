6bc7e9749253c9f5b972e5d57355cd7706e49c70
undx <egallois@talend.com>
2022-11-08 15:57:56 +0100

    chore(maintenance/1.38): bump to 1.38.7-SNAPSHOT

--- .jenkins/scripts/post-release.sh
+++ .jenkins/scripts/post-release.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #
-#  Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+#  Copyright (C) 2006-2022 Talend Inc. - www.talend.com
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
