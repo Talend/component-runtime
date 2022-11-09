7fe28173a8d977c6e13a288e8a8e083190867b7e
wang wei <wwang@talend.com>
2022-11-09 18:10:05 +0800

    fix(TCOMP-2297): TCK - Outline - Flow variable (#591)

--- component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/extension/ReturnVariableMetadataEnricher.java
+++ component-runtime-manager/src/main/java/org/talend/sdk/component/runtime/manager/extension/ReturnVariableMetadataEnricher.java
@@ -25,24 +25,24 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.talend.sdk.component.api.component.AfterVariables;
-import org.talend.sdk.component.api.component.AfterVariables.AfterVariable;
+import org.talend.sdk.component.api.component.ReturnVariables;
+import org.talend.sdk.component.api.component.ReturnVariables.ReturnVariable;
 import org.talend.sdk.component.api.input.Emitter;
 import org.talend.sdk.component.api.input.PartitionMapper;
 import org.talend.sdk.component.api.processor.Processor;
+import org.talend.sdk.component.api.standalone.DriverRunner;
 import org.talend.sdk.component.spi.component.ComponentMetadataEnricher;
 
 /**
- * Search annotation {@link AfterVariable} and add a new meta information about description of after variables.
+ * Search annotation {@link ReturnVariable} and add a new meta information about description of return variables.
  * NOTE. This functionality is used only in Studio.
  */
-@Deprecated
-public class AfterVariableMetadataEnricher implements ComponentMetadataEnricher {
+public class ReturnVariableMetadataEnricher implements ComponentMetadataEnricher {
 
     private static final Set<Class<? extends Annotation>> SUPPORTED_ANNOTATIONS =
-            new HashSet<>(Arrays.asList(PartitionMapper.class, Emitter.class, Processor.class));
+            new HashSet<>(Arrays.asList(PartitionMapper.class, Emitter.class, Processor.class, DriverRunner.class));
 
-    public static final String META_KEY_AFTER_VARIABLE = "variables::after::value";
+    public static final String META_KEY_RETURN_VARIABLE = "variables::return::value";
 
     private static final String VALUE_DELIMITER = "\\:";
 
@@ -56,27 +56,28 @@ public class AfterVariableMetadataEnricher implements ComponentMetadataEnricher 
             return Collections.emptyMap();
         }
 
-        String afterVariableMetaValue = Stream
+        String returnVariableMetaValue = Stream
                 .concat(Stream.of(annotations),
                         Stream
                                 .of(annotations)
-                                .filter(a -> a.annotationType().equals(AfterVariables.class))
-                                .map(AfterVariables.class::cast)
-                                .map(AfterVariables::value)
+                                .filter(a -> a.annotationType().equals(ReturnVariables.class))
+                                .map(ReturnVariables.class::cast)
+                                .map(ReturnVariables::value)
                                 .flatMap(Stream::of))
-                .filter(a -> a.annotationType().equals(AfterVariable.class))
-                .map(AfterVariable.class::cast)
-                .map(AfterVariableMetadataEnricher::makeAfterVariableString)
+                .filter(a -> a.annotationType().equals(ReturnVariable.class))
+                .map(ReturnVariable.class::cast)
+                .map(ReturnVariableMetadataEnricher::makeReturnVariableString)
                 .collect(Collectors.joining(LINE_DELIMITER));
 
-        if (afterVariableMetaValue.isEmpty()) {
+        if (returnVariableMetaValue.isEmpty()) {
             return Collections.emptyMap();
         }
 
-        return Collections.singletonMap(META_KEY_AFTER_VARIABLE, afterVariableMetaValue);
+        return Collections.singletonMap(META_KEY_RETURN_VARIABLE, returnVariableMetaValue);
     }
 
-    private static String makeAfterVariableString(final AfterVariable a) {
-        return a.value() + VALUE_DELIMITER + a.type().getCanonicalName() + VALUE_DELIMITER + a.description();
+    private static String makeReturnVariableString(final ReturnVariable var) {
+        return var.value() + VALUE_DELIMITER + var.type().getCanonicalName() + VALUE_DELIMITER
+                + var.availability().getKey() + VALUE_DELIMITER + var.description();
     }
 }
