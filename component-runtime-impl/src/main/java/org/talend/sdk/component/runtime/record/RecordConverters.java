0bc6cf64eb5283ed6d4997fd1eba809a4bec54fa
wang wei <wwang@talend.com>
2023-01-18 18:54:46 +0800

    fix(TCOMP-2165): [TCK]: tck connector need studio TIS license, as it hard depend on routines.system.Dynamic class (#679)
    * use getMethod instead of getDeclaredMethod
    * feat(TCOMP-2165): add TOS error message
    Co-authored-by: undx <egallois@talend.com>

--- component-runtime-impl/src/main/java/org/talend/sdk/component/runtime/record/RecordConverters.java
+++ component-runtime-impl/src/main/java/org/talend/sdk/component/runtime/record/RecordConverters.java
@@ -489,8 +489,11 @@ public class RecordConverters implements Serializable {
                     constructor.setAccessible(true);
                     recordVisitor = constructor.newInstance(rowStruct, metadata);
                     visitRecord = visitorClass.getDeclaredMethod("visit", Record.class);
-                } catch (final ClassNotFoundException | InstantiationException | IllegalAccessException
-                        | InvocationTargetException | NoSuchMethodException e) {
+                } catch (final NoClassDefFoundError | ClassNotFoundException | InstantiationException
+                        | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    if (e.getMessage().matches(".*routines.system.Dynamic.*")) {
+                        throw new IllegalStateException("TOS does not support dynamic type", e);
+                    }
                     throw new IllegalStateException(e);
                 }
             }
@@ -509,9 +512,12 @@ public class RecordConverters implements Serializable {
                     final Constructor<?> constructor = visitorClass.getConstructors()[0];
                     constructor.setAccessible(true);
                     rowStructVisitor = constructor.newInstance();
-                    visitRowStruct = visitorClass.getDeclaredMethod("get", Object.class, RecordBuilderFactory.class);
-                } catch (final ClassNotFoundException | InstantiationException | IllegalAccessException
-                        | InvocationTargetException | NoSuchMethodException e) {
+                    visitRowStruct = visitorClass.getMethod("get", Object.class, RecordBuilderFactory.class);
+                } catch (final NoClassDefFoundError | ClassNotFoundException | InstantiationException
+                        | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    if (e.getMessage().matches(".*routines.system.Dynamic.*")) {
+                        throw new IllegalStateException("TOS does not support dynamic type", e);
+                    }
                     throw new IllegalStateException(e);
                 }
             }
