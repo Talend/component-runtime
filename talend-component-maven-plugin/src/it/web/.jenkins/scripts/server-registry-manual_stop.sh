2e075bce41f6039d99cb17366accca0bd566133c
acatoire <acatoire@talend.com>
2023-03-17 09:36:43 +0100

    chore(TCOMP-2253) add a stop script and bump runtime version

--- talend-component-maven-plugin/src/it/web/.jenkins/scripts/server-registry-manual_stop.sh
+++ talend-component-maven-plugin/src/it/web/.jenkins/scripts/server-registry-manual_stop.sh
@@ -16,7 +16,7 @@
 
 
 function usage(){
-  printf 'Manual server starter, like ci would do.\n'
+  printf 'Manual server stop, like ci would do.\n'
   printf 'For manual testing.\n'
   printf 'Usage : %s [install_dir] [runtime_version] [server_port] [connectors_version]\n' "${0}"
   printf '\n'
@@ -26,14 +26,8 @@ function usage(){
 # Parameters:
 _USER_PATH=~
 _LOCAL_SERVER_TEST_PATH=${1:-"/tmp"}
-_RUNTIME_VERSION=${2:-"1.55.0-SNAPSHOT"}
-_SERVER_PORT=${3:-"8081"}
-_CONNECTORS_VERSION=${4:-"1.41.0"}
 
-_DOWNLOAD_DIR="${_LOCAL_SERVER_TEST_PATH}/test_tck_server/download"
 _INSTALL_DIR="${_LOCAL_SERVER_TEST_PATH}/test_tck_server/install"
-_COVERAGE_DIR="${_LOCAL_SERVER_TEST_PATH}/test_tck_server/coverage"
-
 _SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 
 printf '##############################################\n'
@@ -41,22 +35,6 @@ printf 'Init parameters\n'
 printf '##############################################\n'
 printf "USER_PATH = %s\n" "${_USER_PATH}"
 printf "LOCAL_SERVER_TEST_PATH = %s\n" "${_LOCAL_SERVER_TEST_PATH}"
-printf "RUNTIME_VERSION = %s\n" "${_RUNTIME_VERSION}"
-printf "SERVER_PORT = %s\n" "${_SERVER_PORT}"
-printf "CONNECTORS_VERSION = %s\n" "${_CONNECTORS_VERSION}"
-printf "DOWNLOAD_DIR = %s\n" "${_DOWNLOAD_DIR}"
-printf "INSTALL_DIR = %s\n" "${_INSTALL_DIR}"
-printf "COVERAGE_DIR = %s\n" "${_COVERAGE_DIR}"
 printf "SCRIPT_PATH = %s\n" "${_SCRIPT_PATH}"
 
 "${_SCRIPT_PATH}"/server-registry-stop.sh "${_INSTALL_DIR}"
-
-"${_SCRIPT_PATH}"/server-registry-init.sh "${_DOWNLOAD_DIR}" \
-                                          "${_INSTALL_DIR}" \
-                                          "${_COVERAGE_DIR}" \
-                                          "${_RUNTIME_VERSION}" \
-                                          "${_CONNECTORS_VERSION}" \
-                                          "${_USER_PATH}/.m2/repository"\
-                                          "${_SERVER_PORT}"
-
-"${_SCRIPT_PATH}"/server-registry-start.sh "${_INSTALL_DIR}" "${_COVERAGE_DIR}" "${_SERVER_PORT}"
