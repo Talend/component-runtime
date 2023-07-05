ffaf27a4cfa4efcc065d4968b2ba37f47ff6a690
acatoire <acatoire@talend.com>
2023-07-05 17:13:45 +0200

    chore(TCOMP-1897) Correct the bulk documentation

--- component-server-parent/component-server-api/src/main/java/org/talend/sdk/component/server/api/BulkReadResource.java
+++ component-server-parent/component-server-api/src/main/java/org/talend/sdk/component/server/api/BulkReadResource.java
@@ -44,30 +44,20 @@ public interface BulkReadResource {
     @APIResponse(responseCode = "200", description = "The request payloads.",
             content = @Content(mediaType = APPLICATION_JSON))
     CompletionStage<BulkResponses> bulk(@RequestBody(
-            description = "The requests list as json objects containing a list of request objects.",
-            //example = " " +
-            //"{" +
-            //"\"requests\":[\n" +
-            //  "{\n" +
-            //  "  \"path\": \"/api/v1/component/index\",\n" +
-            //  "  \"verb\" : \"GET\",\n" +
-            //  "  \"headers\" :\n" +
-            //  "  {\n" +
-            //  "    \"accept\": [\"application/json\"],\n" +
-            //  "    \"content-Type\": [\"application/json\"]\n" +
-            //  "  }\n" +
-            //  "},\n" +
-            //  "{\n" +
-            //  "  \"path\": \"/api/v1/configurationtype/index\",\n" +
-            //  "  \"verb\" : \"GET\",\n" +
-            //  "  \"headers\" :\n" +
-            //  "    {\n" +
-            //  "    \"accept\": [\"application/json\"],\n" +
-            //  "    \"content-Type\": [\"application/json\"]\n" +
-            //  "    }\n" +
-            //  "}\n" +
-            //  "]\n" +
-            //"}",
+            description = "The requests list as json objects containing a list of request objects." +
+                          "If yur request contains multiple identifiers, you must use a list of string" +
+                          "example :" +
+                          "`{" +
+                          "\"requests\" : [\n" +
+                          "{\n" +
+                          "  \"path\" : \"/api/v1/component/index\",\n" +
+                          "  \"queryParameters\" : {\"identifiers\" : [\"12345\", \"6789A\"]},\n"+
+                          "  \"verb\" : \"GET\",\n" +
+                          "  \"headers\" : {...},\n" +
+                          "},\n" +
+                          "{ [...]}\n" +
+                          "]\n" +
+                          "}`",
             required = true,
             content = @Content(mediaType = APPLICATION_JSON)
     ) final BulkRequests requests);
