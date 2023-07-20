84dee75d34d821402cc966488fbf4a6c88005661
Fabien Désiles <52663885+fdesiles@users.noreply.github.com>
2023-07-20 14:55:30 +0200

    TEMP TEST

--- sample-parent/sample-connector/src/main/java/org/talend/sdk/component/test/connectors/config/Auth.java
+++ sample-parent/sample-connector/src/main/java/org/talend/sdk/component/test/connectors/config/Auth.java
@@ -32,6 +32,7 @@ import lombok.Data;
         names = GridLayout.FormType.ADVANCED,
         value = { @GridLayout.Row({ "nbAuthRetry" }) })
 public class Auth implements Serializable {
+    //TEMP TEST
 
     @Option
     @Documentation("Doc: default login documentation without Internationalization.")
