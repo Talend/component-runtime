4be5e36097f1ef1cdf18f5e8bcc4b5e9a7aa3252
Emmanuel GALLOIS <egallois@talend.com>
2023-09-14 11:55:25 +0200

    Revert "fix(TCOMP-2374): DIRecordVisitor to fill Studio Dynamic object's column name by tck Schema.Entry.getName which must follow tck name rule (#787)"
    This reverts commit 1a10e2dd3a9ee1f74b4ff21b53ef5cf90d228cba.

--- component-studio/component-runtime-di/src/main/java/org/talend/sdk/component/runtime/di/record/DiRecordVisitor.java
+++ component-studio/component-runtime-di/src/main/java/org/talend/sdk/component/runtime/di/record/DiRecordVisitor.java
@@ -20,7 +20,6 @@ import static java.util.Collections.emptyMap;
 import static java.util.Optional.ofNullable;
 import static java.util.function.UnaryOperator.identity;
 import static java.util.stream.Collectors.toMap;
-import static org.talend.sdk.component.api.record.SchemaProperty.ALLOW_SPECIAL_NAME;
 import static org.talend.sdk.component.api.record.SchemaProperty.IS_KEY;
 import static org.talend.sdk.component.api.record.SchemaProperty.PATTERN;
 import static org.talend.sdk.component.api.record.SchemaProperty.SCALE;
@@ -132,8 +131,6 @@ public class DiRecordVisitor implements RecordVisitor<Object> {
         }
     }
 
-    private boolean allowSpecialName;
-
     public Object visit(final Record record) {
         arrayOfRecordPrefix = "";
         recordPrefix = "";
@@ -148,8 +145,6 @@ public class DiRecordVisitor implements RecordVisitor<Object> {
             dynamic.getDynamic().clearColumnValues();
         }
         if (hasDynamic && (recordFields == null)) {
-            allowSpecialName = Boolean.parseBoolean(record.getSchema().getProp(ALLOW_SPECIAL_NAME));
-
             recordFields =
                     record.getSchema().getAllEntries().filter(t -> t.getType().equals(Type.RECORD)).map(rcdEntry -> {
                         final String root = rcdEntry.getName() + ".";
@@ -218,19 +213,12 @@ public class DiRecordVisitor implements RecordVisitor<Object> {
 
     private DynamicMetadataWrapper generateMetadata(final Entry entry) {
         final DynamicMetadataWrapper metadata = new DynamicMetadataWrapper();
-        // now ALLOW_SPECIAL_NAME not conflict with the supported nested record as only jdbc connector use it and jdbc
-        // connector never support nested record
-        if (allowSpecialName) {
-            metadata.getDynamicMetadata()
-                    .setName(entry.getOriginalFieldName());
-        } else {
-            metadata.getDynamicMetadata()
-                    .setName(recordFields
-                            .stream()
-                            .filter(f -> f.endsWith("." + entry.getName()))
-                            .findFirst()
-                            .orElse(entry.getName()));
-        }
+        metadata.getDynamicMetadata()
+                .setName(recordFields
+                        .stream()
+                        .filter(f -> f.endsWith("." + entry.getName()))
+                        .findFirst()
+                        .orElse(entry.getName()));
         metadata.getDynamicMetadata().setDbName(entry.getOriginalFieldName());
         metadata.getDynamicMetadata().setNullable(entry.isNullable());
         metadata.getDynamicMetadata().setDescription(entry.getComment());
@@ -279,16 +267,11 @@ public class DiRecordVisitor implements RecordVisitor<Object> {
     private void setField(final Entry entry, final Object value) {
         final Field field = fields.get(entry.getName());
         if (hasDynamic && (field == null || dynamicColumn.equals(entry.getName()))) {
-            final String name;
-            if (allowSpecialName) {
-                name = entry.getOriginalFieldName();
-            } else {
-                name = recordFields
-                        .stream()
-                        .filter(f -> f.endsWith("." + entry.getName()))
-                        .findFirst()
-                        .orElse(entry.getName());
-            }
+            final String name = recordFields
+                    .stream()
+                    .filter(f -> f.endsWith("." + entry.getName()))
+                    .findFirst()
+                    .orElse(entry.getName());
             int index = dynamic.getDynamic().getIndex(name);
             final DynamicMetadataWrapper metadata;
             if (index < 0) {
