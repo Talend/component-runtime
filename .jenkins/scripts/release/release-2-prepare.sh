09b6087f7f09adf855571dbf7eeef7feeb3db7a7
Axel CATOIRE <acatoire@talend.com>
2024-02-06 18:04:15 +0100

    https://jira.talendforge.org/browse/TCOMP-2607
    Implement Jenkins CI evolution to be compatible with milestone release process.
    
    What does this PR adds (design/code thoughts)?
    Add a new release jenkins job in ci/Jenkinsfile-release
    Add possibility to release from a fork using parameter scm.repository
    Keep the old job for now, the release part of the build job will be cleaned later
    Split actual release bash script into stages to have a better visualization
    Add some skip in dockerBuild script
    Aligne pod maven version with pom version <mvn.version>
    remove unused/unmaintain .travis.yml file
    fix some indent

---
+++ .jenkins/scripts/release/release-2-prepare.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+#  Copyright (C) 2006-2024 Talend Inc. - www.talend.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+set -xe
+
+# Parameters:
+# $1: release version
+# $2: next version
+# $3: tag name
+# $4: extra build args for all mvn cmd
+main() {
+  local releaseVersion="${1?Missing release version}"; shift
+  local nextVersion="${1?Missing actual project version}"; shift
+  local tagName="${1?Missing actual project version}"; shift
+  local extraBuildParams=("$@")
+
+  printf ">> Maven prepare release %s (next-dev: %s; tag: %s)\n" "${releaseVersion}" "${nextVersion}" "${tagName}"
+
+  local release_profiles="release,ossrh,gpg2"
+
+  mvn release:prepare \
+    help:effective-pom \
+    --define output="effective-pom-prepare.xml" \
+    --batch-mode \
+    --errors \
+    --define tag="${tagName}" \
+    --define releaseVersion="${releaseVersion}" \
+    --define developmentVersion="${nextVersion}" \
+    --define arguments="-DskipTests -DskipITs -Dcheckstyle.skip -Denforcer.skip=true -Drat.skip" \
+    --settings .jenkins/settings.xml \
+    --activate-profiles "$release_profiles" \
+    "${extraBuildParams[@]}"
+}
+
+main "$@"
