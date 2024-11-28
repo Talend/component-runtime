2a82112652231fe280432d5fcf755e57d1653a63
Axel Catoire <axel.catoire@qlik.com>
2024-11-28 19:00:04 +0100

    fix(QTDI-329) revert options Fix trivy https://qlik-dev.atlassian.net/browse/QTDI-329

--- .jenkins/scripts/trivy_build_tck.sh
+++ .jenkins/scripts/trivy_build_tck.sh
@@ -20,23 +20,31 @@ set -xe
 main() {
   echo "Building project with Maven:"
 
-  mvn clean install -pl '!talend-component-maven-plugin' \
-                    --settings .jenkins/settings.xml \
-                    --batch-mode \
-                    --update-snapshots \
-                    --show-version \
-                    --define gpg.skip=true \
-                    --define spotless.apply.skip=true \
-                    --define spotbugs.skip=true \
-                    --define checkstyle.skip=true \
-                    --define rat.skip=true \
-                    --define skipTests \
-                    --define maven.test.skip=true \
-                    --define maven.javadoc.skip=true \
-                    --define enforcer.skip=true \
-                    --define invoker.skip=true"
-                    --define maven.artifact.threads=25 \
-                    --threads 4C"
+  mvn clean install -pl '!talend-component-maven-plugin' --define skipTests \
+                                                    --define gpg.skip=true \
+                                                    --define enforcer.skip=true\
+                                                    --batch-mode \
+                                                    --update-snapshots \
+                                                    --show-version \
+                                                    --settings .jenkins/settings.xml
+
+
+        #           --settings .jenkins/settings.xml \
+        #           --batch-mode \
+        #           --update-snapshots \
+        #           --show-version \
+        #           --define gpg.skip=true \
+        #           --define spotless.apply.skip=true \
+        #           --define spotbugs.skip=true \
+        #           --define checkstyle.skip=true \
+        #           --define rat.skip=true \
+        #           --define skipTests \
+        #           --define maven.test.skip=true \
+        #           --define maven.javadoc.skip=true \
+        #           --define enforcer.skip=true \
+        #           --define invoker.skip=true"
+        #           --define maven.artifact.threads=25 \
+        #           --threads 4C"
 }
 
 main "$@"
