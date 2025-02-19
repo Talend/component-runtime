b86dbd24981d9c3b1381cae2cfcbf7f77ddb7d35
Emmanuel GALLOIS <Emmanuel.gallois@qlik.com>
2025-02-19 13:17:27 +0100

    fix(QTDI-1188): call reset cache endpoint (#989)
    * fix(QTDI-1188): initial commit
    
    * fix(QTDI-1188): change response to 204 - no content
    
    * fix(QTDI-1188): add additional doc
    
    * fix(QTDI-1188): add additional doc rest-resources
    
    * fix(QTDI-1188): remove jira generator
    
    * fix(QTDI-1188): remove unused var
    
    * chore(QTDI-1188) add api test for undx branch (#990)
    
    chore(QTDI-1188) add api test
    https://qlik-dev.atlassian.net/browse/QTDI-1188
    
    * fix(QTDI-1188): change return content
    
    * fix(QTDI-1188): change API test to cover new response
    
    * fix(QTDI-1188): change API test to cover integer test because cache content is random
    
    * fix(QTDI-1188): clearcache endpoint return a json. 
    
    * fix(QTDI-1188): clearcache endpoint return a json.
    
    * fix(QTDI-1188): clearcache endpoint return a json.
    
    * fix(QTDI-1188): change API test to cover json response
    
    * revert: Fix jenkins file default java version to not overwrite systematically the tool version value.
    Will be done in QTDI-1223
    
    * fix(QTDI-1188): fix sonar
    
    * fix(QTDI-1188): code smell + improve TU.
    
    * fix(QTDI-1188): Typo.
    
    ---------
    
    Co-authored-by: undx <ouf@qlik.com>
    Co-authored-by: Axel CATOIRE <axel.catoire@qlik.com>
    Co-authored-by: ypiel <ypiel@talend.com>
    Co-authored-by: Oleksandr Zhelezniak <oleksandr.zhelezniak@qlik.com>
    Co-authored-by: Yves Piel <YIE@qlik.com>

---
+++ component-server-parent/component-server-api/src/main/java/org/talend/sdk/component/server/api/CacheResource.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2006-2025 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.server.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("cache")
+@Tag(name = "Cache", description = "Endpoints related to caches management.")
+public interface CacheResource {
+
+    @GET
+    @Path("clear")
+    @Operation(operationId = "clearCaches", description = "Clear all caches.")
+    @APIResponse(responseCode = "200", description = "Cleared caches.")
+    Response clearCaches();
+}
