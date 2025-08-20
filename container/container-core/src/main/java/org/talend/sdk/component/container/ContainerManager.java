829d8c5fbf02edd220b459448698b5489afaf47a
Emmanuel GALLOIS <Emmanuel.gallois@qlik.com>
2025-08-20 11:06:16 +0200

    feat(QTDI-693): TCK microservice/OSGI build with dynamic dependencies.
    * feat(QTDI-693): expose nested repository member
    
    * feat(QTDI-693): allow nested jar as root repository
    
    * feat(QTDI-693): classes across many scanning.properties
    
    * feat(QTDI-693): classes across many scanning.properties
    
    * feat(QTDI-693): include nested jars in getURLs
    
    * feat(QTDI-693): include nested jars in getURLs
    
    * feat(QTDI-693): fix test
    
    * feat(QTDI-675): pr comment
    
    * chore(QTDI-1826): unblock github action check (#1079)
    
    * add snapshot plugin oss repo
    * remove talend-icon
    * remove org.talend.ui:talend-icon:3.8.0 it's deprecated dependency, and its content is already inside the folder of documentation/src/main/antora/modules/ROOT/assets/images/icons/svg
    
    * feat(QTDI-113): Support Java 17 compilation (#from:936) (#1061)
    
    * feat(QTDI-113): Support Java 17 compilation (#936)
    
    ---------
    
    Co-authored-by: Emmanuel GALLOIS <emmanuel.gallois@qlik.com>
    Co-authored-by: undx <ouf@qlik.com>
    Co-authored-by: Axel CATOIRE <axel.catoire@qlik.com>
    
    * fix(QTDI-1838): register ContainerFinder for studio (#1081)
    
    * it was removed in 2020 and looks like we've missed it, it was added in a lot of modules for Cloud, but I think by mistake it was removed from component-runtime-manager.
    
    * chore(QTDI-1859): improve CI build (#1084)
    
    * nexus creds for deps report
    * use settings.xml for deps report
    
    * feat(QTDI-693): add plugins.properties location property
    
    - fix springboot repackaging to BOOT-INF/ issue
    
    * feat(QTDI-693): fix path for plugins.properties location propert
    
    * feat(QTDI-693): fix property name
    
    * feat(QTDI-693): remove duplicate info log
    
    ---------
    
    Co-authored-by: Oleksandr Zhelezniak <66320870+ozhelezniak-talend@users.noreply.github.com>
    Co-authored-by: yyin <yyin@talend.com>
    Co-authored-by: Axel CATOIRE <axel.catoire@qlik.com>
    Co-authored-by: Yves Piel <yie@qlik.com>

--- container/container-core/src/main/java/org/talend/sdk/component/container/ContainerManager.java
+++ container/container-core/src/main/java/org/talend/sdk/component/container/ContainerManager.java
@@ -29,9 +29,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -110,16 +112,23 @@ public class ContainerManager implements Lifecycle {
         this.logInfoLevelMapping = logInfoLevelMapping;
         this.containerInitializer = containerInitializer;
         this.resolver = dependenciesResolutionConfiguration.getResolver();
-        this.rootRepositoryLocation = ofNullable(dependenciesResolutionConfiguration.getRootRepositoryLocation())
+
+        Path rootRepo = ofNullable(dependenciesResolutionConfiguration.getRootRepositoryLocation())
                 .filter(Files::exists)
-                .orElseGet(() -> PathFactory.get(System.getProperty("user.home", "")).resolve(".m2/repository"));
-
-        if (log.isDebugEnabled()) {
-            log.debug("Using root repository: " + this.rootRepositoryLocation.toAbsolutePath());
+                .orElseGet(() -> PathFactory.get(System.getProperty("user.home")).resolve(".m2/repository"));
+        // if we've defaulted to user home m2 (fallback), we want to check if we're in running in a fatjar
+        if (PathFactory.get(System.getProperty("user.home")).resolve(".m2/repository").equals(rootRepo)) {
+            final URL nested = classLoaderConfiguration.getParent().getResource("MAVEN-INF/repository");
+            if (nested != null) {
+                rootRepo = Paths.get(nested.getFile().replace("file:", ""));
+            }
         }
-
+        this.rootRepositoryLocation = rootRepo;
+        info("Using root repository: " + this.rootRepositoryLocation.toAbsolutePath());
+        final String pluginsLocation = System.getProperty("talend.component.manager.plugins.location",
+                "TALEND-INF/plugins.properties");
         final String nestedPluginMappingResource = ofNullable(classLoaderConfiguration.getNestedPluginMappingResource())
-                .orElse("TALEND-INF/plugins.properties");
+                .orElse(pluginsLocation);
         this.classLoaderConfiguration = new ClassLoaderConfiguration(
                 ofNullable(classLoaderConfiguration.getParent()).orElseGet(ContainerManager.class::getClassLoader),
                 ofNullable(classLoaderConfiguration.getClassesFilter()).orElseGet(() -> name -> true),
@@ -477,8 +486,8 @@ public class ContainerManager implements Lifecycle {
                     ? nestedContainerMapping.getOrDefault(module, module)
                     : module;
             final Path resolved = resolve(moduleLocation);
-            info("Creating module " + moduleLocation + " (from " + module
-                    + (Files.exists(resolved) ? ", location=" + resolved.toAbsolutePath().toString() : "") + ")");
+            info(String.format("Creating module %s (from %s, location=%s)", moduleLocation, module,
+                    resolved.toAbsolutePath()));
             final Stream<Artifact> classpath = Stream
                     .concat(getBuiltInClasspath(moduleLocation),
                             additionalClasspath == null ? Stream.empty() : additionalClasspath.stream());
