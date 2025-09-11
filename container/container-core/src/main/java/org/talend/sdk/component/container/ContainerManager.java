46e6916711fcee409a773ed23a4aa808e993ef9c
Emmanuel GALLOIS <Emmanuel.gallois@qlik.com>
2025-09-11 16:11:30 +0200

    fix(QTDI-1963): revert java 11 method for path

--- container/container-core/src/main/java/org/talend/sdk/component/container/ContainerManager.java
+++ container/container-core/src/main/java/org/talend/sdk/component/container/ContainerManager.java
@@ -29,7 +29,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -120,11 +119,7 @@ public class ContainerManager implements Lifecycle {
         if (PathFactory.get(System.getProperty("user.home")).resolve(".m2/repository").equals(rootRepo)) {
             final URL nested = classLoaderConfiguration.getParent().getResource("MAVEN-INF/repository");
             if (nested != null) {
-                try {
-                    rootRepo = Path.of(nested.toURI());
-                } catch (URISyntaxException e) {
-                    info("Cannot convert MAVEN-INF/repository url to path: " + e.getMessage());
-                }
+                    rootRepo = PathFactory.get(nested.getFile().replace("file:", ""));
             }
         }
         this.rootRepositoryLocation = rootRepo;
