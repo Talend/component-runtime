5ea8e12ba00937d35c6e4aae0f5be4b73f8e5b5c
Yves Piel <yie@qlik.com>
2025-11-03 17:36:35 +0100

    feat(QTDI-2143): Add DynamicDependenciesServiceConfiguration in the sample connector.

--- sample-parent/sample-connector/src/main/java/org/talend/sdk/component/test/connectors/service/DynamicDependenciesService.java
+++ sample-parent/sample-connector/src/main/java/org/talend/sdk/component/test/connectors/service/DynamicDependenciesService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2006-2025 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.talend.sdk.component.test.connectors.service;
 
 import java.util.ArrayList;
@@ -19,7 +34,7 @@ public class DynamicDependenciesService {
      * @return The list of dynamic dependencies.
      */
     @DynamicDependencies
-    public List<String> getDynamicDependencies(@Option("DynDepsConfig") DynamicDependenciesConf conf) {
+    public List<String> getDynamicDependencies(@Option("DynDepsConfig") final DynamicDependenciesConf conf) {
         List<String> dependencies = new ArrayList<>();
 
         for (int version = 1; version <= 3; version++) {
