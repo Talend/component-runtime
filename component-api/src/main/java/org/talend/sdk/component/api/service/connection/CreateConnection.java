1f612ea1472d85da41b3479f0d9512ea9a30d1a8
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2026-07-09 15:43:26 +0300

    doc(QTDI-3156): Rephraze

--- component-api/src/main/java/org/talend/sdk/component/api/service/connection/CreateConnection.java
+++ component-api/src/main/java/org/talend/sdk/component/api/service/connection/CreateConnection.java
@@ -28,8 +28,8 @@ import org.talend.sdk.component.api.service.ActionType;
 @Target(METHOD)
 @Retention(RUNTIME)
 @Documentation("Mark an action works for creating runtime connection, returning a runtime connection object like jdbc connection if database family. "
-        + "Its parameter MUST be a datastore. Datastore is configuration type annotated with @DataStore. "
-        + "The `value` parameter must match the runtime class constructor parameter name of the component configuration class to correctly apply `@ActiveIf` in the Studio UI. "
+        + "Its parameter MUST be a datastore (configuration type annotated with @DataStore) annotated with `@Option`. "
+        + "The `value` parameter of `@Option` must match the runtime class constructor parameter name of the component configuration class to correctly apply `@ActiveIf` in the Studio UI. "
         + "The functionality is for the Studio only, studio will use the runtime connection object when use existed connection, and no effect for cloud platform.")
 public @interface CreateConnection {
 
